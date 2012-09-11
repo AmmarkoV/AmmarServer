c3289564f27e1fb118af51012e938ab93a54d837
Ammar Qammaz <ammarkov@gmail.com>
2012-09-11 19:08:06 +0300

    fixed passwords :)

--- src/AmmServerlib/configuration.c
+++ src/AmmServerlib/configuration.c
@@ -30,14 +30,14 @@ int LoadConfigurationFile(char * conf_file)
 /*Kind of twisted function TODO : improve it :P*/
 int AssignStr(char ** dest , char * source)
 {  //THIS ISN'T WORKING!
-   if (dest!=0) { free(*dest); }
+   if (*dest!=0) { free(*dest); }
    *dest=0;
    if (source==0) { return 1; }
 
    unsigned int source_len=strlen(source);
    *dest = malloc(sizeof(char) * source_len);
 
-   if (*dest!=0) { return 0; }
+   if (*dest==0) { return 0; }
    strncpy(*dest,source,source_len);
    return 1;
 }
