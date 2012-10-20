d885ab22bf8fb61d0ec6bb388d9f16f5c867a774
Ammar Qammaz <ammarkov@gmail.com>
2012-10-20 04:30:40 +0300

    added a script to make the job of mirroring a dynamic site easier..

---
+++ MirrorAmmarServer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#This is a script to help you mirror your dynamic site in order to get convert to static pages that can be served with AmmarServer  
+ wget --mirror -p --html-extension --convert-links -e robots=off -P . $1
+exit 0
