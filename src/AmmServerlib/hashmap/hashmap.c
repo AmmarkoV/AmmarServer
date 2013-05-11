c8178f044eec965d521ff8b88aa7531d788b6c3b
Ammar Qammaz <ammarkov@gmail.com>
2013-05-11 16:41:23 +0300

    implementing more stuff on the new hashmaps that will replace all search mechanisms currnetly used

--- src/AmmServerlib/hashmap/hashmap.c
+++ src/AmmServerlib/hashmap/hashmap.c
@@ -1,4 +1,5 @@
 #include "hashmap.h"
+#include "../tools/logs.h"
 
 /*! djb2
 This algorithm (k=33) was first reported by dan bernstein many years ago in comp.lang.c. another version of this algorithm (now favored by bernstein) uses xor: hash(i) = hash(i - 1) * 33 ^ str[i]; the magic of number 33 (why it works better than many other constants, prime or not) has never been adequately explained.
@@ -23,27 +24,51 @@ unsigned long hashFunction(char *str)
 
 
 
+int hashMap_Grow(struct hashMap * hm,unsigned int growthSize)
+{
+  struct hashMapEntry * newentries = realloc(hm->entries , sizeof(struct hashMapEntry) * ( hm->maxNumberOfEntries + growthSize ) );
+  if (newentries!=0)
+     {
+       hm->entries=newentries;
+       memset(newentries+hm->maxNumberOfEntries,0,growthSize);
+       hm->maxNumberOfEntries += growthSize;
+       return 1;
+     } else
+     {
+       warning("Could not grow hashMap (running out of memory?)");
+     }
+  return 0;
+}
+
 struct hashMap * hashMap_Create(unsigned int initialEntries , unsigned int entryAllocationStep)
 {
-  return 0;
+  struct hashMap * hm = (struct hashMap *)  malloc(sizeof(struct hashMap));
+  if (hm==0)  { error("Could not allocate a new hashmap"); return 0; }
+
+  memset(hm,0,sizeof(struct hashMap));
+
+  hm->entryAllocationStep=entryAllocationStep;
+
+  if (!hashMap_Grow(hm,initialEntries) )
+  {
+    error("Could not grow new hashmap for the first time");
+    free(hm);
+    return 0;
+  }
+
+  return hm;
 }
+
+int hashMap_IsOK(struct hashMap * hm)
+{
+    if (hm == 0)  { return 0; }
+    if (hm->entries == 0)  { return 0; }
+    if (hm->maxNumberOfEntries == 0)  { return 0; }
+    return 1;
+}
+
 
 struct hashMap * hashMap_Clone(struct hashMap * hm)
-{
-  return 0;
-}
-
-void hashMap_Destroy(struct hashMap * hm)
-{
-  return ;
-}
-
-void * hashMap_Add(struct hashMap * hm,char * key,void * val)
-{
-  return 0;
-}
-
-void * hashMap_Get(struct hashMap * hm,char * key)
 {
   return 0;
 }
@@ -53,20 +78,73 @@ void hashMap_Clear(struct hashMap * hm)
   return;
 }
 
+void hashMap_Destroy(struct hashMap * hm)
+{
+  return ;
+}
+
+int hashMap_Add(struct hashMap * hm,char * key,void * val)
+{
+  if (hm->curNumberOfEntries >= hm->maxNumberOfEntries)
+  {
+    if  (!hashMap_Grow(hm,hm->entryAllocationStep))
+    {
+      error("Could not grow new hashmap for adding new values");
+      return 0;
+    }
+  }
+
+  //TODO ADD STUFF HERE :P
+
+
+  return 0;
+}
+
+void * hashMap_Get(struct hashMap * hm,char * key,int * found)
+{
+  *found=0;
+  if (!hashMap_IsOK(hm)) { return 0;}
+  unsigned int i=0;
+  unsigned long keyHash = hashFunction(key);
+  while ( i < hm->curNumberOfEntries )
+  {
+    if ( hm->entries[i].keyHash == keyHash ) { *found=1; return i; }
+    ++i;
+  }
+  return 0;
+}
+
+
 int hashMap_ContainsKey(struct hashMap * hm,char * key)
 {
+  if (!hashMap_IsOK(hm)) { return 0;}
+  unsigned int i=0;
+  unsigned long keyHash = hashFunction(key);
+  while ( i < hm->curNumberOfEntries )
+  {
+    if ( hm->entries[i].keyHash == keyHash ) { return 1; }
+    ++i;
+  }
   return 0;
 }
 
 
 int hashMap_ContainsValue(struct hashMap * hm,void * val)
 {
+  if (!hashMap_IsOK(hm)) { return 0;}
+  unsigned int i=0;
+  while ( i < hm->curNumberOfEntries )
+  {
+    if ( hm->entries[i].payload == val ) { return 1; }
+    ++i;
+  }
   return 0;
 }
 
 
 int hashMap_GetSize(struct hashMap * hm)
 {
+  if (!hashMap_IsOK(hm)) { return 0;}
   return 0;
 }
 
--- src/AmmServerlib/hashmap/hashmap.h
+++ src/AmmServerlib/hashmap/hashmap.h
@@ -17,6 +17,7 @@ struct hashMap
 {
   unsigned int maxNumberOfEntries;
   unsigned int curNumberOfEntries;
+  unsigned int entryAllocationStep;
   struct hashMapEntry * entries;
 };
 
@@ -25,8 +26,8 @@ struct hashMap
 struct hashMap * hashMap_Create(unsigned int initialEntries , unsigned int entryAllocationStep);
 struct hashMap * hashMap_Clone(struct hashMap * hm);
 void hashMap_Destroy(struct hashMap * hm);
-void * hashMap_Add(struct hashMap * hm,char * key,void * val);
-void * hashMap_Get(struct hashMap * hm,char * key);
+int hashMap_Add(struct hashMap * hm,char * key,void * val);
+void * hashMap_Get(struct hashMap * hm,char * key,int * found);
 void hashMap_Clear(struct hashMap * hm);
 int hashMap_ContainsKey(struct hashMap * hm,char * key);
 int hashMap_ContainsValue(struct hashMap * hm,void * val);
