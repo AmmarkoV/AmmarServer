ec414a62bd1f5e7ffee6ac4ead65b1791aa9f5d1
Ammar Qammaz <ammarkov@gmail.com>
2013-07-12 12:13:03 +0300

    added code for starting using parameters , code::blocks refuses to compile in my desktop pc , commiting it commented

--- src/AmmServerlib/version.h
+++ src/AmmServerlib/version.h
@@ -14,17 +14,17 @@
 	//Standard Version Type
 	static const long MAJOR = 0;
 	static const long MINOR = 27;
-	static const long BUILD = 95;
-	static const long REVISION = 351;
+	static const long BUILD = 96;
+	static const long REVISION = 353;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT = 84;
-	#define RC_FILEVERSION 0,27,95,351
-	#define RC_FILEVERSION_STRING "0, 27, 95, 351\0"
-	static const char FULLVERSION_STRING[] = "0.27.95.351";
+	#define RC_FILEVERSION 0,27,96,353
+	#define RC_FILEVERSION_STRING "0, 27, 96, 353\0"
+	static const char FULLVERSION_STRING[] = "0.27.96.353";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 60;
+	static const long BUILD_HISTORY = 61;
 	
 
 #endif //VERSION_H
