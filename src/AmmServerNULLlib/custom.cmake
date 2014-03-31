6c858adeee9784c4281e2579adf39d8d0828f7ed
Ammar Qammaz <ammarkov@gmail.com>
2014-03-31 16:26:39 +0300

    fixing CMakeLists

--- src/AmmServerNULLlib/custom.cmake
+++ src/AmmServerNULLlib/custom.cmake
@@ -1,4 +1,3 @@
-execute_process(COMMAND "pwd && ln -s ../AmmServerlib/AmmServerlib.h"
-    OUTPUT_VARIABLE _output OUTPUT_STRIP_TRAILING_WHITESPACE)
-
+execute_process(COMMAND "ln -s ${CMAKE_CURRENT_SOURCE_DIR}/../AmmServerlib/AmmServerlib.h" )
+# OUTPUT_VARIABLE _output OUTPUT_STRIP_TRAILING_WHITESPACE)
 #file(WRITE custom.h "#define COMPILE_TIME_VALUE \"${_output}\"")
