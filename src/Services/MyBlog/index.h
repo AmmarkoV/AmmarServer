01ce44829ea67e61aff3d6ec8f9d4bb74def6c6a
Ammar Qammaz <ammarkov@gmail.com>
2015-02-18 14:56:28 +0200

    ...

---
+++ src/Services/MyBlog/index.h
@@ -0,0 +1,11 @@
+#ifndef INDEX_H_INCLUDED
+#define INDEX_H_INCLUDED
+
+
+#include "../../AmmServerlib/AmmServerlib.h"
+
+void * prepare_index(struct AmmServer_DynamicRequest  * rqst);
+
+
+
+#endif // INDEX_H_INCLUDED
