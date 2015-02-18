cdb54ad138612ff46fa54978c7dbeb7a8bb12605
Ammar Qammaz <ammarkov@gmail.com>
2015-02-18 13:31:36 +0200

    ...

--- src/Services/MyBlog/main.c
+++ src/Services/MyBlog/main.c
@@ -42,6 +42,7 @@ struct AmmServer_RH_Context stats={0};
 void * prepare_index(struct AmmServer_DynamicRequest  * rqst)
 {
   //No range check but since everything here is static max_stats_size should be big enough not to segfault with the strcat calls!
+  /*
   snprintf(rqst->content,rqst->MAXcontentSize,
            "<html><head><title>Dynamic Content Enabled</title><meta http-equiv=\"refresh\" content=\"1\"></head>\
             <body>The date and time in AmmarServer is<br><h2>%02d-%02d-%02d %02d:%02d:%02d\n</h2>\
@@ -50,7 +51,7 @@ void * prepare_index(struct AmmServer_DynamicRequest  * rqst)
             Dynamic content code label in ammarserver main.c</a><br>\n\
             If you dont need dynamic content at all consider disabling it from ammServ.conf or by setting DYNAMIC_CONTENT_RESOURCE_MAPPING_ENABLED=0; in \
             <a href=\"https://github.com/AmmarkoV/AmmarServer/blob/master/src/AmmServerlib/file_caching.c\">file_caching.c</a> and recompiling.!</body></html>",
-           tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900,   tm.tm_hour, tm.tm_min, tm.tm_sec);
+           tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900,   tm.tm_hour, tm.tm_min, tm.tm_sec);*/
 
   rqst->contentSize=strlen(rqst->content);
   return 0;
