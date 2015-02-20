e1a3d6c172a279d6b0a165847004a85385d108dc
Ammar Qammaz <ammarkov@gmail.com>
2015-02-20 11:51:45 +0200

    directory lists now hiding hidden files from indexing , they are still accessible though..! ( I am not sure whether they should or shouldn't be )

--- src/AmmServerlib/version.h
+++ src/AmmServerlib/version.h
@@ -13,18 +13,18 @@
 	
 	//Standard Version Type
 	static const long MAJOR  = 0;
-	static const long MINOR  = 28;
-	static const long BUILD  = 234;
-	static const long REVISION  = 1168;
+	static const long MINOR  = 29;
+	static const long BUILD  = 237;
+	static const long REVISION  = 1183;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 231;
-	#define RC_FILEVERSION 0,28,234,1168
-	#define RC_FILEVERSION_STRING "0, 28, 234, 1168\0"
-	static const char FULLVERSION_STRING [] = "0.28.234.1168";
+	static const long BUILDS_COUNT  = 233;
+	#define RC_FILEVERSION 0,29,237,1183
+	#define RC_FILEVERSION_STRING "0, 29, 237, 1183\0"
+	static const char FULLVERSION_STRING [] = "0.29.237.1183";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 99;
+	static const long BUILD_HISTORY  = 2;
 	
 
 #endif //VERSION_H
