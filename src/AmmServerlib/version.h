7b02178f9b5177b444f26bce410a44ad9a9dc135
Ammar Qammaz <ammarkov@gmail.com>
2015-03-17 20:35:42 +0200

    fixing MyBlog injection..

--- src/AmmServerlib/version.h
+++ src/AmmServerlib/version.h
@@ -2,7 +2,7 @@
 #define VERSION_H
 
 	//Date Version Types
-	static const char DATE[] = "07";
+	static const char DATE[] = "17";
 	static const char MONTH[] = "03";
 	static const char YEAR[] = "2015";
 	static const char UBUNTU_VERSION_STYLE[] =  "15.03";
@@ -14,17 +14,17 @@
 	//Standard Version Type
 	static const long MAJOR  = 0;
 	static const long MINOR  = 29;
-	static const long BUILD  = 263;
-	static const long REVISION  = 1316;
+	static const long BUILD  = 265;
+	static const long REVISION  = 1334;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 267;
-	#define RC_FILEVERSION 0,29,263,1316
-	#define RC_FILEVERSION_STRING "0, 29, 263, 1316\0"
-	static const char FULLVERSION_STRING [] = "0.29.263.1316";
+	static const long BUILDS_COUNT  = 268;
+	#define RC_FILEVERSION 0,29,265,1334
+	#define RC_FILEVERSION_STRING "0, 29, 265, 1334\0"
+	static const char FULLVERSION_STRING [] = "0.29.265.1334";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 28;
+	static const long BUILD_HISTORY  = 30;
 	
 
 #endif //VERSION_H
