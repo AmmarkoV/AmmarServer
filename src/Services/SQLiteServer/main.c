2d33b16396d61686bc196db99e25034011d17e43
Ammar Qammaz <ammarkov@gmail.com>
2015-07-21 12:25:09 +0300

    forming SQLite Sample..

--- src/Services/SQLiteServer/main.c
+++ src/Services/SQLiteServer/main.c
@@ -1,10 +1,311 @@
+/*
+AmmarServer , SQLite executable
+
+URLs: http://ammar.gr
+Written by Ammar Qammaz a.k.a. AmmarkoV 2015
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <sqlite3.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include "../../AmmServerlib/AmmServerlib.h"
 
-int main(void) {
+#define DEFAULT_BINDING_PORT 8080  // <--- Change this to 80 if you want to bind to the default http port..!
+
+char webserver_root[MAX_FILE_PATH]="public_html/"; // <- change this to the directory that contains your content if you dont want to use the default public_html dir..
+char templates_root[MAX_FILE_PATH]="public_html/templates/";
 
 
-    printf("%s\n", sqlite3_libversion());
+/*! Dynamic content code ..! START!*/
+/* A few words about dynamic content here..
+   This is actually one of the key features on AmmarServer and maybe the reason that I started the whole project
+   What I am trying to do here is serve content by directly linking the webserver to binary ( NOT Interpreted ) code
+   in order to serve pages with the maximum possible efficiency and skipping all intermediate layers..
+
+   PHP , Ruby , Python and all other "web-languages" are very nice and handy and to be honest I can do most of my work fine using PHP , MySQL and Apache
+   However setting up , configuring and maintaining large projects with different database systems , separate configuration files for each of the sub parts
+   and re deploying everything is a very tiresome affair.. Not to mention that despite the great work done by the apache  , PHP etc teams performance is wasted
+   due to the interpreters of the various scripting languages used..
+
+   Things can't get any faster than AmmarServer and the whole programming interface exposed to the programmer is ( imho ) very friendly and familiar to even inexperienced
+   C developer..
+
+   What follows is the decleration of some "Dynamic Content Resources" their Constructors/Destructors and their callback routines that fill them with the content to be served
+   each time a client requests one of the pages..
+*/
+
+//The decleration of some dynamic content resources..
+struct AmmServer_Instance  * default_server=0;
+struct AmmServer_RequestOverride_Context GET_override={{0}};
+
+struct AmmServer_RH_Context random_chars={0};
+struct AmmServer_RH_Context stats={0};
+
+
+//This function prepares the content of  stats context , ( stats.content )
+void * prepare_stats_content_callback(struct AmmServer_DynamicRequest  * rqst)
+{
+  time_t t = time(NULL);
+  struct tm tm = *localtime(&t);
+
+  fprintf(stderr,"Trying to write dynamic request to %p , max size %lu \n",rqst->content , rqst->MAXcontentSize);
+
+  //No range check but since everything here is static max_stats_size should be big enough not to segfault with the strcat calls!
+  snprintf(rqst->content,rqst->MAXcontentSize,
+           "<html><head><title>Dynamic Content Enabled</title><meta http-equiv=\"refresh\" content=\"1\"></head>\
+            <body>The date and time in AmmarServer is<br><h2>%02d-%02d-%02d %02d:%02d:%02d\n</h2>\
+            The string you see is updated dynamically every time you get a fresh copy of this file!<br><br>\n\
+            To include your own content see the <a href=\"https://github.com/AmmarkoV/AmmarServer/blob/master/src/main.c#L46\">\
+            Dynamic content code label in ammarserver main.c</a><br>\n\
+            If you dont need dynamic content at all consider disabling it from ammServ.conf or by setting DYNAMIC_CONTENT_RESOURCE_MAPPING_ENABLED=0; in \
+            <a href=\"https://github.com/AmmarkoV/AmmarServer/blob/master/src/AmmServerlib/file_caching.c\">file_caching.c</a> and recompiling.!</body></html>",
+           tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900,   tm.tm_hour, tm.tm_min, tm.tm_sec);
+
+  rqst->contentSize=strlen(rqst->content);
+  return 0;
+}
+
+
+//This function prepares the content of  random_chars context , ( random_chars.content )
+void * prepare_random_content_callback(struct AmmServer_DynamicRequest  * rqst)
+{
+  //No range check but since everything here is static max_stats_size should be big enough not to segfault with the strcat calls!
+  strncpy(rqst->content,"<html><head><title>Random Number Generator</title><meta http-equiv=\"refresh\" content=\"1\"></head><body>",rqst->MAXcontentSize);
+
+  char hex[16+1]={0};
+  unsigned int i=0;
+  for (i=0; i<1024; i++)
+    {
+        snprintf(hex,16, "%x ", rand()%256 );
+        strcat(rqst->content,hex);
+    }
+
+  strcat(rqst->content,"</body></html>");
+
+  rqst->contentSize=strlen(rqst->content);
+  return 0;
+}
+
+
+//This function could alter the content of the URI requested and then return 1
+void request_override_callback(void * request)
+{
+  //struct AmmServer_RequestOverride_Context * rqstContext = (struct AmmServer_RequestOverride_Context *) request;
+  return;
+}
+
+//This function adds a Resource Handler for the pages stats.html and formtest.html and associates stats , form and their callback functions
+void init_dynamic_content()
+{
+  AmmServer_AddRequestHandler(default_server,&GET_override,"GET",&request_override_callback);
+
+  if (! AmmServer_AddResourceHandler(default_server,&stats,"/stats.html",webserver_root,4096,0,&prepare_stats_content_callback,SAME_PAGE_FOR_ALL_CLIENTS) )
+     { AmmServer_Warning("Failed adding stats page\n"); }
+
+   if (! AmmServer_AddResourceHandler(default_server,&random_chars,"/random.html",webserver_root,4096,0,&prepare_random_content_callback,DIFFERENT_PAGE_FOR_EACH_CLIENT) )
+     { AmmServer_Warning("Failed adding random testing page\n"); }
+
+}
+
+//This function destroys all Resource Handlers and free's all allocated memory..!
+void close_dynamic_content()
+{
+    AmmServer_RemoveResourceHandler(default_server,&stats,1);
+}
+/*! Dynamic content code ..! END ------------------------*/
+
+
+
+
+
+
+int printCars(void *NotUsed, int argc, char **argv,
+                    char **azColName)
+{
+    int i = 0;
+    NotUsed = 0;
+
+    for (i = 0; i < argc; i++) {
+
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+
+    printf("\n");
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+int printCars(void *, int, char **, char **);
+
+
+
+
+int main(int argc, char *argv[])
+{
+
+    sqlite3 *db;
+    sqlite3_stmt *res;
+    char *err_msg = 0;
+
+    int rc = sqlite3_open("test.db", &db);
+
+    if (rc != SQLITE_OK) {
+
+        fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+
+        return 1;
+    }
+
+
+
+
+    rc = sqlite3_prepare_v2(db, "SELECT SQLITE_VERSION()", -1, &res, 0);
+
+    if (rc != SQLITE_OK) {
+
+        fprintf(stderr, "Failed to fetch data: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+
+        return 1;
+    }
+
+    rc = sqlite3_step(res);
+
+    if (rc == SQLITE_ROW) {
+        printf("%s\n", sqlite3_column_text(res, 0));
+    }
+
+    sqlite3_finalize(res);
+
+
+
+
+
+    char *sql = "DROP TABLE IF EXISTS Cars;"
+                "CREATE TABLE Cars(Id INT, Name TEXT, Price INT);"
+                "INSERT INTO Cars VALUES(1, 'Audi', 52642);"
+                "INSERT INTO Cars VALUES(2, 'Mercedes', 57127);"
+                "INSERT INTO Cars VALUES(3, 'Skoda', 9000);"
+                "INSERT INTO Cars VALUES(4, 'Volvo', 29000);"
+                "INSERT INTO Cars VALUES(5, 'Bentley', 350000);"
+                "INSERT INTO Cars VALUES(6, 'Citroen', 21000);"
+                "INSERT INTO Cars VALUES(7, 'Hummer', 41400);"
+                "INSERT INTO Cars VALUES(8, 'Volkswagen', 21600);";
+
+    rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
+
+    if (rc != SQLITE_OK ) {
+
+        fprintf(stderr, "SQL error: %s\n", err_msg);
+
+        sqlite3_free(err_msg);
+        sqlite3_close(db);
+
+        return 1;
+    }
+
+
+
+
+
+    char *sqlSelect = "SELECT * FROM Cars";
+    rc = sqlite3_exec(db, sqlSelect, printCars, 0, &err_msg);
+
+    if (rc != SQLITE_OK ) {
+
+        fprintf(stderr, "Failed to select data\n");
+        fprintf(stderr, "SQL error: %s\n", err_msg);
+
+        sqlite3_free(err_msg);
+        sqlite3_close(db);
+
+        return 1;
+    }
+
+
+
+
+
+    printf("\nAmmar Server %s starting up..\n",AmmServer_Version());
+    //Check binary and header spec
+    AmmServer_CheckIfHeaderBinaryAreTheSame(AMMAR_SERVER_HTTP_HEADER_SPEC);
+    //Register termination signal for when we receive SIGKILL etc
+    AmmServer_RegisterTerminationSignal(&close_dynamic_content);
+
+    char bindIP[MAX_IP_STRING_SIZE];
+    strcpy(bindIP,"0.0.0.0");
+
+    unsigned int port=DEFAULT_BINDING_PORT;
+
+    //Kick start AmmarServer , bind the ports , create the threads and get things going..!
+    default_server = AmmServer_StartWithArgs(
+                                             "SimpleTemplate",
+                                              argc,argv , //The internal server will use the arguments to change settings
+                                              //If you don't want this look at the AmmServer_Start call
+                                              bindIP,
+                                              port,
+                                              0, /*This means we don't want a specific configuration file*/
+                                              webserver_root,
+                                              templates_root
+                                              );
+
+
+    if (!default_server) { AmmServer_Error("Could not start server , shutting down everything.."); exit(1); }
+
+    //Create dynamic content allocations and associate context to the correct files
+    init_dynamic_content();
+    //stats.html and formtest.html should be availiable from now on..!
+
+         while ( (AmmServer_Running(default_server))  )
+           {
+             //Main thread should just sleep and let the background threads do the hard work..!
+             //In other applications the programmer could use the main thread to do anything he likes..
+             //The only caveat is that he would takeup more CPU time from the server and that he would have to poll
+             //the AmmServer_Running() call once in a while to make sure everything is in order
+             //usleep(60000);
+             sleep(1);
+           }
+
+    //Delete dynamic content allocations and remove stats.html and formtest.html from the server
+    close_dynamic_content();
+
+    //Stop the server and clean state
+    AmmServer_Stop(default_server);
+    AmmServer_Warning("Ammar Server stopped\n");
+    return 0;
+
+
+
+
+
+
+
+
+
+    sqlite3_close(db);
 
     return 0;
 }
