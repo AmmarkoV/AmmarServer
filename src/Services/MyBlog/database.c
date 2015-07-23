e24add775be0adaf9a6a89ec577c451902225f5c
Ammar Qammaz <ammarkov@gmail.com>
2015-07-23 14:25:22 +0300

    tying posts to sqlite

--- src/Services/MyBlog/database.c
+++ src/Services/MyBlog/database.c
@@ -10,21 +10,6 @@ struct website myblog={0};
 struct SQLiteSession sqlserver={0};
 
 
-int printCars(void *rqstV, int argc, char **argv, char **azColName)
-{
-    struct AmmServer_DynamicRequest  * rqst = (struct AmmServer_DynamicRequest  *) rqstV;
-    char formatBuffer[512]={0};
-
-    unsigned int i=0;
-    for (i = 0; i < argc; i++)
-    {
-        snprintf(formatBuffer,512,"<tr><td>%s</td><td>%s</td></tr>\n", azColName[i], argv[i] ? argv[i] : "NULL");
-        strcat(rqst->content,formatBuffer);
-    }
-
-   return 0;
-}
-
 int SQL_init(struct SQLiteSession * sqlserver , const char * dbFilename)
 {
     if (sqlite3_config(SQLITE_CONFIG_SERIALIZED)!=SQLITE_OK)
@@ -74,45 +59,36 @@ int SQL_getVersion(struct SQLiteSession * sqlserver)
 }
 
 
-int SQL_populate(struct SQLiteSession * sqlserver )
+int appendPosts(void *rqstV, int argc, char **argv, char **azColName)
 {
-    char *sql = "DROP TABLE IF EXISTS Cars;"
-                "CREATE TABLE Cars(Id INT, Name TEXT, Price INT);"
-                "INSERT INTO Cars VALUES(1, 'Audi', 52642);"
-                "INSERT INTO Cars VALUES(2, 'Mercedes', 57127);"
-                "INSERT INTO Cars VALUES(3, 'Skoda', 9000);"
-                "INSERT INTO Cars VALUES(4, 'Volvo', 29000);"
-                "INSERT INTO Cars VALUES(5, 'Bentley', 350000);"
-                "INSERT INTO Cars VALUES(6, 'Citroen', 21000);"
-                "INSERT INTO Cars VALUES(7, 'Hummer', 41400);"
-                "INSERT INTO Cars VALUES(8, 'Volkswagen', 21600);";
+    struct website * websiteContext = (struct website *) rqstV;
 
-    sqlserver->rc = sqlite3_exec(sqlserver->db, sql, 0, 0, &sqlserver->err_msg);
 
-    if (sqlserver->rc != SQLITE_OK )
+    unsigned int postID = websiteContext->post.currentPosts;
+    unsigned int i=0;
+    for (i = 0; i < argc; i++)
     {
-
-        fprintf(stderr, "SQL error: %s\n", sqlserver->err_msg);
-
-        sqlite3_free(sqlserver->err_msg);
-        sqlite3_close(sqlserver->db);
-
-        return 0;
+        if (strcmp(azColName[i],"author")==0)  { strncpy( websiteContext->post.item[postID].author   , argv[i] ? argv[i] : "NULL" , MAX_STR );   } else
+        if (strcmp(azColName[i],"title")==0)   { strncpy( websiteContext->post.item[postID].title    , argv[i] ? argv[i] : "NULL" , MAX_STR );   } else
+        if (strcmp(azColName[i],"date")==0)    { strncpy( websiteContext->post.item[postID].dateStr  , argv[i] ? argv[i] : "NULL" , MAX_STR );   } else
+        if (strcmp(azColName[i],"content")==0) {
+                                                 websiteContext->post.item[postID].content.totalDataLength = strlen(argv[i]);
+                                                 websiteContext->post.item[postID].content.currentDataLength = websiteContext->post.item[postID].content.totalDataLength;
+                                                 websiteContext->post.item[postID].content.data = (char * ) malloc(sizeof(char) * (1+websiteContext->post.item[postID].content.totalDataLength)  );
+                                                 strncpy( websiteContext->post.item[postID].content.data  , argv[i] , websiteContext->post.item[postID].content.totalDataLength );
+                                               }
     }
 
- return 1;
+
+    ++websiteContext->post.currentPosts;
+   return 0;
 }
 
 
-int serveCarsPageWithSQL(struct SQLiteSession * sqlserver , struct AmmServer_DynamicRequest  * rqst)
+int loadPostsFromSQL(struct SQLiteSession * sqlserver , struct website * websiteContext)
 {
-    strncpy(rqst->content,"<html><head><title>Car List</title></head><body><table>",rqst->MAXcontentSize);
-
-    char *sqlSelect = "SELECT * FROM Cars";
-    sqlserver->rc = sqlite3_exec(sqlserver->db, sqlSelect, printCars, (void*) rqst, &sqlserver->err_msg);
-
-    strcat(rqst->content,"</table></body></html>");
-    rqst->contentSize=strlen(rqst->content);
+    char *sqlSelect = "SELECT title,date,author,content FROM posts";
+    sqlserver->rc = sqlite3_exec(sqlserver->db, sqlSelect, appendPosts, (void*) websiteContext, &sqlserver->err_msg);
 
     if (sqlserver->rc != SQLITE_OK )
     {
@@ -125,63 +101,9 @@ int serveCarsPageWithSQL(struct SQLiteSession * sqlserver , struct AmmServer_Dyn
      return 0;
     }
 
-
-
   return 1;
 }
 
-
-
-
-
-int serveCarsPageWithSQL(struct SQLiteSession * sqlserver , struct AmmServer_DynamicRequest  * rqst)
-{
-    strncpy(rqst->content,"<html><head><title>Car List</title></head><body><table>",rqst->MAXcontentSize);
-
-    char *sqlSelect = "SELECT * FROM Cars";
-    sqlserver->rc = sqlite3_exec(sqlserver->db, sqlSelect, printCars, (void*) rqst, &sqlserver->err_msg);
-
-    strcat(rqst->content,"</table></body></html>");
-    rqst->contentSize=strlen(rqst->content);
-
-    if (sqlserver->rc != SQLITE_OK )
-    {
-     fprintf(stderr, "Failed to select data\n");
-     fprintf(stderr, "SQL error: %s\n", sqlserver->err_msg);
-
-     sqlite3_free(sqlserver->err_msg);
-     sqlite3_close(sqlserver->db);
-
-     return 0;
-    }
-
-
-
-  return 1;
-}
-
-
-
-
-
-
-/*
-   struct menuItemList menu;
-   struct linkItemList linksLeft;
-   struct linkItemList linksRight;
-   struct postItemList post;
-
-   struct widgetItemList widget;
-
-struct postItem
-{
-  unsigned char title[MAX_STR];
-  unsigned char dateStr[MAX_STR];
-  unsigned char author[MAX_STR];
-  struct tagItemList tags;
-  struct htmlContent content;
-};
-*/
 
 
 
