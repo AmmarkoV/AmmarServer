950b7fed0eed1b5a7c5d7245eedc50025a2de530
Ammar Qammaz <ammarkov@gmail.com>
2015-08-15 16:21:36 +0300

    thumbnails everywhere..

--- src/Services/MyTube/thumbnailer.c
+++ src/Services/MyTube/thumbnailer.c
@@ -1,8 +1,41 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "thumbnailer.h"
+#include "../../AmmServerlib/AmmServerlib.h"
 
 
-int generateThumbnailOfVideo()
+char * generateThumbnailOfVideo(const char * videoDirectory,const char * videofile,const char * thumbDirectory)
 {
-    //ffmpeg -i "Wilkinson - Afterglow(1).mp4" -ss 00:00:14.435 -vframes 1 out.jpg
+   unsigned int thumbDirectoryLength=strlen(thumbDirectory);
+   unsigned int videoFileLength=strlen(videofile);
 
+   unsigned int thumbnailFileLength=thumbDirectoryLength+videoFileLength+25;
+   char * thumbnailFile = (char*) malloc(sizeof(char) * thumbnailFileLength);
+
+   if (thumbnailFile!=0)
+   {
+    if (AmmServer_FileExists(thumbnailFile))
+    {
+      return thumbnailFile;
+    }
+
+    snprintf(thumbnailFile,thumbnailFileLength,"%s/%s_thumb.jpg",thumbDirectory,videofile);
+    char what2Execute[1024];
+    snprintf(what2Execute,1024,"ffmpeg -y -i \"%s/%s\" -ss 00:00:14.435 -vframes 1 -vf scale=320:240 \"%s\" ",videoDirectory,videofile,thumbnailFile);
+
+    fprintf(stderr,"Spawning %s .. ",thumbnailFile);
+    int i=system(what2Execute);
+
+    if (i==0)
+    {
+       fprintf(stderr,"success \n");
+       return thumbnailFile;
+    }
+    fprintf(stderr,"failed\n");
+
+     free(thumbnailFile);
+   }
+  return 0;
 }
