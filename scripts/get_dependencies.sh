8a9e579c4b294754356e4ab820fc859c4ade1a69
Ammar Qammaz <ammarkov@gmail.com>
2015-08-24 11:52:29 +0300

    added sqlite to dependencies..

--- scripts/get_dependencies.sh
+++ scripts/get_dependencies.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 
-sudo apt-get install liblzma-dev libjpeg-turbo8-dev build-essential cmake
+sudo apt-get install liblzma-dev libjpeg-turbo8-dev build-essential cmake libsqlite3-dev 
+
 
 exit 0
