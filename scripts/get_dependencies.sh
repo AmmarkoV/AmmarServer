d58fb0b2dae4a9d52161747d38efb67316d91d26
Ammar Qammaz <ammarkov@gmail.com>
2015-08-24 11:54:17 +0300

    added dependency for remote desktop stuff..

--- scripts/get_dependencies.sh
+++ scripts/get_dependencies.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 
-sudo apt-get install liblzma-dev libjpeg-turbo8-dev build-essential cmake libsqlite3-dev 
+
+sudo apt-get install liblzma-dev libjpeg-turbo8-dev build-essential cmake libsqlite3-dev  libxt-dev
 
 
 exit 0
