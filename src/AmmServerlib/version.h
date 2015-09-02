46e5a2b4056cc1cbd597780c4f4a87de848f63f5
Ammar Qammaz <ammarkov@gmail.com>
2015-09-03 02:13:20 +0300

    file server cleaning..

--- src/AmmServerlib/version.h
+++ src/AmmServerlib/version.h
@@ -2,10 +2,10 @@
 #define VERSION_H
 
 	//Date Version Types
-	static const char DATE[] = "19";
-	static const char MONTH[] = "08";
+	static const char DATE[] = "03";
+	static const char MONTH[] = "09";
 	static const char YEAR[] = "2015";
-	static const char UBUNTU_VERSION_STYLE[] =  "15.08";
+	static const char UBUNTU_VERSION_STYLE[] =  "15.09";
 	
 	//Software Status
 	static const char STATUS[] =  "Alpha";
@@ -14,17 +14,17 @@
 	//Standard Version Type
 	static const long MAJOR  = 0;
 	static const long MINOR  = 29;
-	static const long BUILD  = 299;
-	static const long REVISION  = 1497;
+	static const long BUILD  = 302;
+	static const long REVISION  = 1508;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 317;
-	#define RC_FILEVERSION 0,29,299,1497
-	#define RC_FILEVERSION_STRING "0, 29, 299, 1497\0"
-	static const char FULLVERSION_STRING [] = "0.29.299.1497";
+	static const long BUILDS_COUNT  = 321;
+	#define RC_FILEVERSION 0,29,302,1508
+	#define RC_FILEVERSION_STRING "0, 29, 302, 1508\0"
+	static const char FULLVERSION_STRING [] = "0.29.302.1508";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 64;
+	static const long BUILD_HISTORY  = 67;
 	
 
 #endif //VERSION_H
