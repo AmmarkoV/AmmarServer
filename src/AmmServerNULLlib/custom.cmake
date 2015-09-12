c1d560da17a34735faedf52f5e9e8bf795ae8ddb
Ammar Qammaz <ammarkov@gmail.com>
2015-09-12 23:57:53 +0300

    thrown the null version of the library to the garbage , it does not serve any puropose with the modern CMake build system..

--- src/AmmServerNULLlib/custom.cmake
+++
@@ -1,3 +0,0 @@
-execute_process(COMMAND "ln -s ${CMAKE_CURRENT_SOURCE_DIR}/../AmmServerlib/AmmServerlib.h" )
-# OUTPUT_VARIABLE _output OUTPUT_STRIP_TRAILING_WHITESPACE)
-#file(WRITE custom.h "#define COMPILE_TIME_VALUE \"${_output}\"")
