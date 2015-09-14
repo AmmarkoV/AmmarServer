5ad31c5c3437ef9113f962dacc76283595d3ecac
Ammar Qammaz <ammarkov@gmail.com>
2015-09-14 17:49:01 +0300

    rewritting inject function , it is broken atm..

--- src/AmmServerlib/version.h
+++ src/AmmServerlib/version.h
@@ -2,7 +2,7 @@
 #define VERSION_H
 
 	//Date Version Types
-	static const char DATE[] = "13";
+	static const char DATE[] = "14";
 	static const char MONTH[] = "09";
 	static const char YEAR[] = "2015";
 	static const char UBUNTU_VERSION_STYLE[] =  "15.09";
@@ -14,17 +14,17 @@
 	//Standard Version Type
 	static const long MAJOR  = 0;
 	static const long MINOR  = 30;
-	static const long BUILD  = 346;
-	static const long REVISION  = 1751;
+	static const long BUILD  = 355;
+	static const long REVISION  = 1801;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 376;
-	#define RC_FILEVERSION 0,30,346,1751
-	#define RC_FILEVERSION_STRING "0, 30, 346, 1751\0"
-	static const char FULLVERSION_STRING [] = "0.30.346.1751";
+	static const long BUILDS_COUNT  = 387;
+	#define RC_FILEVERSION 0,30,355,1801
+	#define RC_FILEVERSION_STRING "0, 30, 355, 1801\0"
+	static const char FULLVERSION_STRING [] = "0.30.355.1801";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 11;
+	static const long BUILD_HISTORY  = 20;
 	
 
 #endif //VERSION_H
