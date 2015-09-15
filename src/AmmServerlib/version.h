d986fb0301ba7a19e1b3df778fe3bdaac333fe65
Ammar Qammaz <ammarkov@gmail.com>
2015-09-15 17:56:22 +0300

    valgrind still whines about invalid read but I am beggining to suspect this is due to the realloc and the way valgrind works internally..

--- src/AmmServerlib/version.h
+++ src/AmmServerlib/version.h
@@ -2,7 +2,7 @@
 #define VERSION_H
 
 	//Date Version Types
-	static const char DATE[] = "14";
+	static const char DATE[] = "15";
 	static const char MONTH[] = "09";
 	static const char YEAR[] = "2015";
 	static const char UBUNTU_VERSION_STYLE[] =  "15.09";
@@ -14,17 +14,17 @@
 	//Standard Version Type
 	static const long MAJOR  = 0;
 	static const long MINOR  = 30;
-	static const long BUILD  = 358;
-	static const long REVISION  = 1815;
+	static const long BUILD  = 360;
+	static const long REVISION  = 1824;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 389;
-	#define RC_FILEVERSION 0,30,358,1815
-	#define RC_FILEVERSION_STRING "0, 30, 358, 1815\0"
-	static const char FULLVERSION_STRING [] = "0.30.358.1815";
+	static const long BUILDS_COUNT  = 390;
+	#define RC_FILEVERSION 0,30,360,1824
+	#define RC_FILEVERSION_STRING "0, 30, 360, 1824\0"
+	static const char FULLVERSION_STRING [] = "0.30.360.1824";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 23;
+	static const long BUILD_HISTORY  = 25;
 	
 
 #endif //VERSION_H
