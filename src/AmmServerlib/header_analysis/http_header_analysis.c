a51e912147123b0b6059c0e522a711b2f38d8ae1
Ammar Qammaz <ammarkov@gmail.com>
2015-09-16 18:08:40 +0300

    ...

--- src/AmmServerlib/header_analysis/http_header_analysis.c
+++ src/AmmServerlib/header_analysis/http_header_analysis.c
@@ -84,7 +84,7 @@ char * ReceiveHTTPHeader(struct AmmServer_Instance * instance,int clientSock , u
   //Gather Header until http request contains two newlines..!
   fprintf(stderr,"recv called ( socket %u )\n",clientSock);
    //usleep(1000);
-   opres=recv(clientSock,&incomingRequest[incomingRequestLength],MAXincomingRequestLength-incomingRequestLength,/*MSG_DONTWAIT*/);
+   opres=recv(clientSock,&incomingRequest[incomingRequestLength],MAXincomingRequestLength-incomingRequestLength,0/*MSG_DONTWAIT*/);
   fprintf(stderr,"recv returned ( socket %u )\n",clientSock);
   if (opres<0)
    {
