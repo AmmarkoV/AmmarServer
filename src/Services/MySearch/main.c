5b485f3f203c7c4a7889b60c458d1f2a773fd137
Ammar Qammaz <ammarkov@gmail.com>
2015-09-17 16:59:39 +0300

    tying search input

--- src/Services/MySearch/main.c
+++ src/Services/MySearch/main.c
@@ -30,56 +30,48 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 char webserver_root[MAX_FILE_PATH]="public_html/"; // <- change this to the directory that contains your content if you dont want to use the default public_html dir..
 char templates_root[MAX_FILE_PATH]="public_html/templates/";
 
-
-/*! Dynamic content code ..! START!*/
-/* A few words about dynamic content here..
-   This is actually one of the key features on AmmarServer and maybe the reason that I started the whole project
-   What I am trying to do here is serve content by directly linking the webserver to binary ( NOT Interpreted ) code
-   in order to serve pages with the maximum possible efficiency and skipping all intermediate layers..
-
-   PHP , Ruby , Python and all other "web-languages" are very nice and handy and to be honest I can do most of my work fine using PHP , MySQL and Apache
-   However setting up , configuring and maintaining large projects with different database systems , separate configuration files for each of the sub parts
-   and re deploying everything is a very tiresome affair.. Not to mention that despite the great work done by the apache  , PHP etc teams performance is wasted
-   due to the interpreters of the various scripting languages used..
-
-   Things can't get any faster than AmmarServer and the whole programming interface exposed to the programmer is ( imho ) very friendly and familiar to even inexperienced
-   C developer..
-
-   What follows is the decleration of some "Dynamic Content Resources" their Constructors/Destructors and their callback routines that fill them with the content to be served
-   each time a client requests one of the pages..
-*/
-
 //The decleration of some dynamic content resources..
 struct AmmServer_Instance  * default_server=0;
 struct AmmServer_RequestOverride_Context GET_override={{0}};
 
-
 struct AmmServer_RH_Context indexContext={0};
 struct AmmServer_RH_Context searchContext={0};
 struct AmmServer_RH_Context logoContext={0};
+struct AmmServer_RH_Context faviconContext={0};
 
 
-//This function prepares the content of  stats context , ( stats.content )
 void * prepare_logo_content_callback(struct AmmServer_DynamicRequest  * rqst)
 {
-
-  if (!AmmServer_DynamicRequestReturnFile(rqst,"src/Services/MySearch/search.png") )
-   {
-       AmmServer_Error("Could not return default thumbnail");
-   }
-
+  if (!AmmServer_DynamicRequestReturnFile(rqst,"src/Services/MySearch/res/search.png") ) { AmmServer_Error("Could not return default thumbnail"); }
   rqst->contentSize=strlen(rqst->content);
   return 0;
 }
 
-//This function prepares the content of  stats context , ( stats.content )
 void * prepare_index_content_callback(struct AmmServer_DynamicRequest  * rqst)
 {
+  if (!AmmServer_DynamicRequestReturnFile(rqst,"src/Services/MySearch/res/search.html") ) { AmmServer_Error("Could not return default thumbnail"); }
+  rqst->contentSize=strlen(rqst->content);
+  return 0;
+}
 
-  if (!AmmServer_DynamicRequestReturnFile(rqst,"src/Services/MySearch/search.html") )
-   {
-       AmmServer_Error("Could not return default thumbnail");
-   }
+void * favicon_callback(struct AmmServer_DynamicRequest  * rqst)
+{
+  if (!AmmServer_DynamicRequestReturnFile(rqst,"src/Services/MySearch/res/favicon.ico") )  { AmmServer_Error("Could not return favicon");  }
+  rqst->contentSize=strlen(rqst->content);
+  return 0;
+}
+
+
+void * search_callback(struct AmmServer_DynamicRequest  * rqst)
+{
+  char query[512]={0};
+  if ( _GET(default_server,rqst,"q",query,512) )
+              {
+               snprintf(rqst->content,rqst->MAXcontentSize,"<!DOCTYPE html>\n<html><head><meta http-equiv=\"refresh\" content=\"5;URL='index.html'\" /></head><body>Search query `%s`</body></html>",query);
+              } else
+              {
+               snprintf(rqst->content,rqst->MAXcontentSize,"<!DOCTYPE html>\n<html><head><meta http-equiv=\"refresh\" content=\"0;URL='index.html'\" /></head><body>Search</body></html>");
+              }
 
   rqst->contentSize=strlen(rqst->content);
   return 0;
@@ -88,11 +80,19 @@ void * prepare_index_content_callback(struct AmmServer_DynamicRequest  * rqst)
 //This function adds a Resource Handler for the pages stats.html and formtest.html and associates stats , form and their callback functions
 void init_dynamic_content()
 {
+
+  if (! AmmServer_AddResourceHandler(default_server,&searchContext,"/search.html",webserver_root,4096,0,&search_callback,SAME_PAGE_FOR_ALL_CLIENTS) )
+     { AmmServer_Warning("Failed adding stats page\n"); }
+
   if (! AmmServer_AddResourceHandler(default_server,&indexContext,"/index.html",webserver_root,4096,0,&prepare_index_content_callback,SAME_PAGE_FOR_ALL_CLIENTS) )
      { AmmServer_Warning("Failed adding stats page\n"); }
 
    if (! AmmServer_AddResourceHandler(default_server,&logoContext,"/search.png",webserver_root,4096,0,&prepare_logo_content_callback,DIFFERENT_PAGE_FOR_EACH_CLIENT) )
      { AmmServer_Warning("Failed adding random testing page\n"); }
+
+   if (! AmmServer_AddResourceHandler(default_server,&faviconContext,"/favicon.ico",webserver_root,4096,0,&favicon_callback,DIFFERENT_PAGE_FOR_EACH_CLIENT) )
+     { AmmServer_Warning("Failed adding random testing page\n"); }
+
 
 }
 
@@ -101,8 +101,8 @@ void close_dynamic_content()
 {
     AmmServer_RemoveResourceHandler(default_server,&indexContext,1);
     AmmServer_RemoveResourceHandler(default_server,&logoContext,1);
+    AmmServer_RemoveResourceHandler(default_server,&faviconContext,1);
 }
-/*! Dynamic content code ..! END ------------------------*/
 
 
 
