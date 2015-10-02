ca2ee037f89e4b51fc7ae1a769ca709d78b6ae13
Ammar Qammaz <ammarkov@gmail.com>
2015-10-02 21:28:52 +0300

    splitting recv code..

---
+++ src/AmmServerlib/network/recvHTTPHeader.h
@@ -0,0 +1,14 @@
+/** @file recvHTTPHeader.h
+* @brief Small code segments that receive HTTP responses
+* @author Ammar Qammaz (AmmarkoV)
+*/
+
+#ifndef RECEIVEHTTPHEADER_H_INCLUDED
+#define RECEIVEHTTPHEADER_H_INCLUDED
+
+
+#include "../AmmServerlib.h"
+
+receiveAndParseIncomingHTTPRequest(struct AmmServer_Instance * instance,struct HTTPTransaction * transaction,int clientSock);
+
+#endif // RECEIVEHTTPHEADER_H_INCLUDED
--- src/AmmServerlib/network/sendHTTPHeader.c
+++ src/AmmServerlib/network/sendHTTPHeader.c
@@ -1,4 +1,3 @@
-
 #include "sendHTTPHeader.h"
 
 #include <stdio.h>
