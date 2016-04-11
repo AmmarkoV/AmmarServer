4fb073ae8f760e86729b7b2d513ae37b1b3d987e
Ammar Qammaz <ammarkov@gmail.com>
2016-04-11 03:38:34 +0300

    mytube has a CSS file that is stupidly served now

--- src/Services/MyTube/renderVideoList.c
+++ src/Services/MyTube/renderVideoList.c
@@ -45,10 +45,7 @@ int renderVideoList(struct videoCollection *  db ,struct AmmServer_MemoryHandler
            <head>\
             <meta charset=\"UTF-8\">\
             <title>MyTube Listing %s</title>\
-            <style>\
-             a { color: #000000; }\
-             hr { border: 0; height: 0; border-top: 1px solid rgba(0, 0, 0, 0.1); border-bottom: 1px solid rgba(255, 255, 255, 0.3); } \
-            </style>\
+            <link rel=\"stylesheet\" type=\"text/css\" href=\"mytube.css\">\
          </head>\
          <body>",query);
   dataSize=strlen(data);
