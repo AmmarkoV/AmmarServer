da339ee27f53fa8207272468375821ead45e6b83
Ammar Qammaz <ammarkov@gmail.com>
2016-08-23 18:53:31 +0300

    ...

--- src/Services/MyBlog/database.c
+++ src/Services/MyBlog/database.c
@@ -9,10 +9,12 @@
 struct website myblog={0};
 
 
-int loadPostInfo(struct website * configuration,unsigned int postNum )
+int loadPostInfo(struct website * configuration,unsigned int postNum)
 {
+ if (postNum==0) { fprintf(stderr,"Wrong enumeration on post info \n"); return 0; }
  char filename[FILENAME_MAX]={0};
  snprintf(filename,FILENAME_MAX,"src/Services/MyBlog/res/posts/info%u.html",postNum);
+ fprintf(stderr," Loading post info %u (%s) .. \n",postNum,filename);
 
  ssize_t read;
 
@@ -20,7 +22,10 @@ int loadPostInfo(struct website * configuration,unsigned int postNum )
  FILE * fp = fopen(filename,"r");
  if (fp!=0)
   {
-   struct InputParserC * ipc = InputParser_Create(512,5);
+   struct InputParserC * ipc = InputParser_Create(512,4);
+   InputParser_SetDelimeter(ipc,1,'(');
+   InputParser_SetDelimeter(ipc,2,',');
+   InputParser_SetDelimeter(ipc,3,')');
 
    struct tagItemList tags;
    char * line = NULL;
@@ -28,18 +33,21 @@ int loadPostInfo(struct website * configuration,unsigned int postNum )
 
     while ((read = getline(&line, &len, fp)) != -1)
     {
-       printf("Retrieved line of length %zu :\n", read);
-       printf("%s", line);
+       //printf("Retrieved line of length %zu :\n", read);
+       //printf("%s", line);
+       InputParser_SeperateWords(ipc,line,0);
 
-       if ( InputParser_WordCompareNoCaseAuto(ipc,0,"TITLE")  )   { InputParser_GetWord(ipc,1,configuration->post.item[postNum].title  ,MAX_STR);  printf("=> TITLE \n");  } else
-       if ( InputParser_WordCompareNoCaseAuto(ipc,0,"DATE")   )   { InputParser_GetWord(ipc,1,configuration->post.item[postNum].dateStr,MAX_STR);  printf("=> DATE \n");   } else
-       if ( InputParser_WordCompareNoCaseAuto(ipc,0,"AUTHOR") )   { InputParser_GetWord(ipc,1,configuration->post.item[postNum].author ,MAX_STR);  printf("=> AUTHOR \n"); } else
-        {
-
-        }
-       //if ( InputParser_WordCompareNoCaseAuto(ipc,0,"TAGS") )     { InputParser_GetWord(ipc,0,configuration->post.item[postNum].tags ,MAX_STR); } else
+       if ( InputParser_WordCompareNoCaseAuto(ipc,0,"TITLE")  )   { InputParser_GetWord(ipc,1,configuration->post.item[postNum-1].title  ,MAX_STR);   } else
+       if ( InputParser_WordCompareNoCaseAuto(ipc,0,"DATE")   )   { InputParser_GetWord(ipc,1,configuration->post.item[postNum-1].dateStr,MAX_STR);   } else
+       if ( InputParser_WordCompareNoCaseAuto(ipc,0,"AUTHOR") )   { InputParser_GetWord(ipc,1,configuration->post.item[postNum-1].author ,MAX_STR);   }
+       //if ( InputParser_WordCompareNoCaseAuto(ipc,0,"TAGS") )     { InputParser_GetWord(ipc,0,configuration->post.item[postNum-1].tags ,MAX_STR); } else
        //if ( InputParser_WordCompareNoCaseAuto(ipc,0,"COMMENTS") ) { InputParser_GetWord(ipc,0,line,MAX_STR); }
     }
+
+    fprintf(stderr," Post Info %u --------------\n",postNum);
+    fprintf(stderr,"   Title : %s \n",configuration->post.item[postNum-1].title);
+    fprintf(stderr,"   Date : %s \n",configuration->post.item[postNum-1].dateStr);
+    fprintf(stderr,"   Author : %s \n",configuration->post.item[postNum-1].author);
 
     fclose(fp);
     if (line) { free(line); }
@@ -73,7 +81,7 @@ int loadPosts(struct website * configuration)
     configuration->post.item[configuration->post.currentPosts].content.data = tmp->content;
     //If we didnt use this we shold -> AmmServer_FreeMemoryHandler(&tmp); tmp->content=0;
 
-    loadPostInfo(configuration,configuration->post.currentPosts);
+    loadPostInfo(configuration,number);
     //-------------
     ++configuration->post.currentPosts;
    }
