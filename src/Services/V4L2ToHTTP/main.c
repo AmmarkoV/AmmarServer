31e99dfd61dd88f97f09be06f10da17cf2f4ff70
Ammar Qammaz <ammarkov@gmail.com>
2017-01-16 15:13:51 +0200

    merging and updating the codebase from https://github.com/AmmarkoV/V4L2ToHTTP so that in the future it will be included in AmmarServer builds..

---
+++ src/Services/V4L2ToHTTP/main.c
@@ -0,0 +1,294 @@
+/*
+V4L2ToHTTP , main executable
+
+URLs: http://ammar.gr
+Written by Ammar Qammaz a.k.a. AmmarkoV 2012
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <signal.h>
+#include "../../AmmServerlib/AmmServerlib.h"
+
+#define MAX_BINDING_PORT 65534
+#define DEFAULT_BINDING_PORT 8081
+#define MAX_INPUT_IP 256
+
+
+
+char webcam[MAX_FILE_PATH]="/dev/video0";
+char webserver_root[MAX_FILE_PATH]="public_html/";
+char templates_root[MAX_FILE_PATH]="public_html/templates/";
+
+pthread_mutex_t refresh_jpeg_lock;
+
+struct AmmServer_Instance * v4l2_server=0;
+
+struct AmmServer_RH_Context index_page= {0};
+
+struct AmmServer_RH_Context jpeg_picture= {0};
+unsigned int jpg_width=VIDEO_WIDTH,jpg_height=VIDEO_HEIGHT;
+
+
+void * prepare_index_page_callback(struct AmmServer_DynamicRequest * rqst)
+{
+  if (SIMPLE_INDEX)
+    {
+      strcpy(rqst->content,"<html><head><meta http-equiv=\"refresh\" content=\"1\"><title>V4L2ToHTTP</title></head><body><br><br><center><img src=\"cam.jpg\"><br><h3><a href=\"index.html\">-- Manually Reload Page --</a></h3></center></body></html>");
+    }
+  else
+    {
+      strcpy(rqst->content,"<html>\n");
+      strcat(rqst->content,"    <head>\n");
+      strcat(rqst->content,"         <title>V4L2ToHTTP</title>\n");
+      strcat(rqst->content,"         <script language=\"JavaScript\"><!--\n");
+
+      strcat(rqst->content,"                var refreshDelayMilliSecs = ");
+
+      char webupstr[128]={0};
+      sprintf(webupstr,"%u\n",WEB_PAGE_UPDATE_EVERY_MS);
+      strcat(rqst->content,webupstr);
+
+      strcat(rqst->content,"                var newImage = new Image();\n");
+      strcat(rqst->content,"                var number = 0;\n");
+      strcat(rqst->content,"                newImage.src = \"cam.jpg?i=0\";\n");
+      strcat(rqst->content,"                 function updateImage()\n");
+      strcat(rqst->content,"                    {\n");
+      strcat(rqst->content,"                           if(newImage.complete)\n");
+      strcat(rqst->content,"                           {\n");
+      strcat(rqst->content,"                                document.getElementById(\"LiveImage\").src = newImage.src;\n");
+      strcat(rqst->content,"                                if(newImage.complete)\n");
+      strcat(rqst->content,"                                 {\n");
+      strcat(rqst->content,"                                      document.getElementById(\"LiveImage\").src = newImage.src;\n");
+      strcat(rqst->content,"                                      newImage = new Image();\n");
+      strcat(rqst->content,"                                      number++;\n");
+      strcat(rqst->content,"                                     newImage.src = \"cam.jpg?i=\"+number\n"); //Later on make it cam.jpg?inc=\" + number;
+      strcat(rqst->content,"                                   }\n");
+      strcat(rqst->content,"                        }\n                     setTimeout('updateImage()',refreshDelayMilliSecs);\n");
+      strcat(rqst->content,"                   }\n");
+      strcat(rqst->content,"               //--></script>\n");
+      strcat(rqst->content,"</head>\n");
+      strcat(rqst->content,"<body  onLoad=\"setTimeout('updateImage()',refreshDelayMilliSecs)\">\n");
+      strcat(rqst->content,"<br><br><center><img src=\"cam.jpg?i=0\" id=\"LiveImage\"><br>");
+      strcat(rqst->content,"<h5>Active Clients : ");
+
+      if (AmmServer_GetInfo(v4l2_server,AMMINF_ACTIVE_CLIENTS) < 10000 )
+           {
+             char viewers_str[150]={0};
+             sprintf(viewers_str,"%u",AmmServer_GetInfo(v4l2_server,AMMINF_ACTIVE_CLIENTS));
+             strcat(rqst->content,viewers_str);
+           } else
+           {
+               strcat(rqst->content,"N/A");
+           }
+
+      strcat(rqst->content,"<br>\n");
+      strcat(rqst->content,"<br><a href=\"index.html\">-- Manually Reload Page --</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\n");
+      strcat(rqst->content,"<a href=\"https://github.com/AmmarkoV/V4L2ToHTTP\">-- Get V4L2ToHTTP --</a></h5></center></body></html>\n");
+    }
+
+  rqst->MAXcontentSize=strlen((char *)rqst->content);
+  rqst->contentSize=rqst->MAXcontentSize;
+  return 0;
+}
+
+
+void * prepare_camera_data_callback(struct AmmServer_DynamicRequest * rqst)
+{
+  if (VideoSimulationState()!=LIVE_ON)
+    {
+      fprintf(stderr,"Warning : Camera already snapping\n");
+      rqst->contentSize=0;
+      return 0;
+    }
+
+  pthread_mutex_lock (&refresh_jpeg_lock); // LOCK PROTECTED OPERATION -------------------------------------------
+
+  fprintf(stderr,"Calling Camera callback \n");
+  rqst->contentSize=jpg_width * jpg_height * 3; //Signal the max allocated buffer , this value will be changed by RecordOneInMem
+
+  unsigned long jpeg_size_refreshed=0;
+  VideoInput_SaveFrameJPEGMemory( 0 , rqst->content , &jpeg_size_refreshed);
+
+
+  rqst->contentSize  =  jpeg_size_refreshed;
+  fprintf(stderr,"Calling Camera callback success new picture ( %u bytes long ) ready !\n",(unsigned int) rqst->contentSize);
+
+  pthread_mutex_unlock (&refresh_jpeg_lock); // LOCK PROTECTED OPERATION -------------------------------------------
+  return 0;
+}
+
+
+
+int open_camera(char * webcam_dev,unsigned int width,unsigned int height,unsigned int framerate)
+{
+  if ( !VideoInput_InitializeLibrary(1) )
+    {
+      fprintf(stderr,"Could not open Video Input\n");
+      return 0;
+    }
+
+  char BITRATE=32;
+  struct VideoFeedSettings feedsettings= {0};
+  //videosettings.PixelFormat=V4L2_PIX_FMT_YUYV; BITRATE=16; // <- Common compressed setting for UVC webcams
+  feedsettings.PixelFormat=V4L2_PIX_FMT_RGB24;
+  BITRATE=24;   //   <- Common raw setting for UVC webcams ( Run Compat )
+  jpg_width=width;
+  jpg_height=height;
+
+  if (! VideoInput_OpenFeed(0,webcam_dev,jpg_width,jpg_height,BITRATE,framerate,1,feedsettings) )
+    {
+      fprintf(stderr,"Could not set Video feed settings consider running with v4l2convert.so preloaded\n");
+      return 0;
+    }
+
+  int MAX_waittime=10000;
+  int waittime=0;
+  while ( ( !VideoInput_FeedReceiveLoopAlive(0) )&& (waittime<MAX_waittime) )
+    {
+      ++waittime;
+    }
+
+  return 1;
+}
+
+int close_camera()
+{
+  if (jpeg_picture.requestContext.content!=0)
+    {
+      free(jpeg_picture.requestContext.content);
+      jpeg_picture.requestContext.content=0;
+    }
+  VideoInput_DeinitializeLibrary();
+  return 1;
+}
+
+void init_dynamic_pages()
+{
+  //int AmmServer_AddResourceHandler(struct AmmServer_RH_Context * context, char * resource_name , char * web_root, unsigned int allocate_mem_bytes,unsigned int callback_every_x_msec,void * callback);
+  AmmServer_AddResourceHandler(v4l2_server,&index_page,(char *) "/index.html",webserver_root,4096,0,(void *) &prepare_index_page_callback,SAME_PAGE_FOR_ALL_CLIENTS);
+
+  //Do not empty jpeg_picture struct since mallocs have already happened.. memset(&jpeg_picture,0,sizeof(struct AmmServer_RH_Context));
+
+  AmmServer_AddResourceHandler(v4l2_server,&jpeg_picture,(char *) "/cam.jpg",webserver_root,jpg_width * jpg_height * 3, 250 /*Poll camera no sooner than once every x ms*/,(void *) &prepare_camera_data_callback,DIFFERENT_PAGE_FOR_EACH_CLIENT);
+  jpeg_picture.requestContext.contentSize=jpg_width * jpg_height * 3;
+  jpeg_picture.requestContext.MAXcontentSize=jpg_width * jpg_height * 3;
+
+  prepare_camera_data_callback(0); //Do a callback to populate content..!
+
+  AmmServer_DoNOTCacheResourceHandler(v4l2_server,&jpeg_picture);  // Cam.jpg will be constantly changing , do not send 304 NOT MODIFIED
+}
+
+void close_dynamic_pages()
+{
+
+}
+
+
+void termination_handler (int signum)
+     {
+        fprintf(stderr,"Terminating V4L2ToHTTP.. ");
+        close_dynamic_pages();
+        AmmServer_Stop(v4l2_server);
+        fprintf(stderr,"done\n");
+        exit(0);
+     }
+
+
+
+
+int main(int argc, char *argv[])
+{
+  printf("V4L2ToHTTP starting up linked to Ammar Server v%s\n",AmmServer_Version());
+  if (signal(SIGINT, termination_handler) == SIG_ERR)   printf("Cannot handle SIGINT!\n");
+  if (signal(SIGHUP, termination_handler) == SIG_ERR)   printf("Cannot handle SIGHUP!\n");
+  if (signal(SIGTERM, termination_handler) == SIG_ERR)  printf("Cannot handle SIGTERM!\n");
+  if (signal(SIGKILL, termination_handler) == SIG_ERR)  printf("Cannot handle SIGKILL!\n");
+
+  char bindIP[MAX_INPUT_IP];
+  strcpy(bindIP,"0.0.0.0");
+
+  unsigned int port=DEFAULT_BINDING_PORT;
+
+  if ( argc <1 )
+    {
+      fprintf(stderr,"Something weird is happening , argument zero should be executable path :S \n");
+      return 1;
+    }
+  else if ( argc <= 2 )
+    {
+      /*fprintf(stderr,"Please note that you may choose a different binding IP/port as command line parameters.. \"ammarserver 0.0.0.0 8080\"\n");*/
+    }
+  else
+    {
+      if (strlen(argv[1])>=MAX_INPUT_IP)
+        {
+          fprintf(stderr,"Console argument for binding IP is too long..!\n");
+        }
+      else
+        {
+          strncpy(bindIP,argv[1],MAX_INPUT_IP);
+        }
+      port=atoi(argv[2]);
+      if (port>=MAX_BINDING_PORT)
+        {
+          port=DEFAULT_BINDING_PORT;
+        }
+    }
+  if (argc>=3)
+    {
+      strncpy(webcam,argv[3],MAX_FILE_PATH);
+    }
+
+  if ( open_camera(webcam,VIDEO_WIDTH,VIDEO_HEIGHT,FRAMERATE) )
+    {
+      //We were able to start camera so lets start everything else
+
+   v4l2_server = AmmServer_Start("v4l2http",bindIP,port,0,webserver_root,templates_root);
+   if (!v4l2_server) { fprintf(stderr,"Could not start webserver closing everything.."); close_camera(); exit(1); }
+
+  if (ENABLE_PASSWORD_PROTECTION)
+   {
+     AmmServer_SetStrSettingValue(v4l2_server,AMMSET_USERNAME_STR,(char*) "admin");
+     AmmServer_SetStrSettingValue(v4l2_server,AMMSET_PASSWORD_STR,(char*) "admin"); //these 2 calls should change BASE64PASSWORD to the proper hex value
+    /* It is best to enable password protection after correctly setting both username and password
+       to avoid the rare race condition of logging only with username ( i.e. when password hasn't been declared */
+     AmmServer_SetIntSettingValue(v4l2_server,AMMSET_PASSWORD_PROTECTION,1);
+   }
+
+      init_dynamic_pages(); // Map index.html , cam.jpg to their content and callbacks
+
+      while (AmmServer_Running(v4l2_server))
+        {
+          //Serving pages on background..!
+          usleep(10000);
+        }
+
+      close_dynamic_pages(); // Free mappings of AmmServer
+
+      close_camera();
+
+      AmmServer_Stop(v4l2_server);
+    }
+
+
+  fprintf(stderr,"Closing application\n");
+  return 0;
+}
---
+++ src/Services/V4L2ToHTTP/v4l2_acquisition_shared_library/V4L2Wrapper.c
@@ -0,0 +1,326 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "PixelFormatConversions.h"
+#include "V4L2Wrapper.h"
+
+char video_simulation_path[256]={0};
+
+
+int total_cameras=0;
+unsigned char * empty_frame=0;
+unsigned int largest_feed_x=640;
+unsigned int largest_feed_y=480;
+
+struct Video * camera_feeds=0;
+
+
+io_method io=IO_METHOD_MMAP;//  /*IO_METHOD_MMAP;  IO_METHOD_READ; IO_METHOD_USERPTR;*/
+
+
+
+int DecodePixels(int webcam_id)
+{
+ if ( camera_feeds[webcam_id].frame_decoded==0)
+     { /*THIS FRAME HASN`T BEEN DECODED YET!*/
+       int i=Convert2RGB24( (unsigned char*)camera_feeds[webcam_id].frame,
+                            (unsigned char*)camera_feeds[webcam_id].decoded_pixels,
+                            camera_feeds[webcam_id].width,
+                            camera_feeds[webcam_id].height,
+                            camera_feeds[webcam_id].input_pixel_format,
+                            camera_feeds[webcam_id].input_pixel_format_bitdepth );
+       if ( i == 0 ) { /* UNABLE TO PERFORM CONVERSION */ return 0; } else
+                     { /* SUCCESSFUL CONVERSION */
+                       camera_feeds[webcam_id].frame_decoded=1;
+                     }
+    }
+ return 1;
+}
+
+
+unsigned char * ReturnDecodedLiveFrame(int webcam_id)
+{
+   /*
+          THIS FRAME DECIDES IF THE VIDEO FORMAT NEEDS DECODING OR CAN BE RETURNED RAW FROM THE DEVICE
+          SEE PixelFormats.cpp / PixelFormatConversions.cpp
+   */
+
+   if (VideoFormatNeedsDecoding(camera_feeds[webcam_id].input_pixel_format,camera_feeds[webcam_id].input_pixel_format_bitdepth)==1)
+    {
+     /*VIDEO COMES IN A FORMAT THAT NEEDS DECODING TO RGB 24*/
+     if ( DecodePixels(webcam_id)==0 ) return empty_frame;
+     return (unsigned char *) camera_feeds[webcam_id].decoded_pixels;
+    } else
+    {
+      /* The frame is ready so we mark it as decoded*/
+      camera_feeds[webcam_id].frame_decoded=1;
+      if ( camera_feeds[webcam_id].frame == 0 )
+         {
+           /*Handler for when the frame does not exist */
+           return empty_frame;
+         }
+     return (unsigned char *) camera_feeds[webcam_id].frame;
+    }
+   return empty_frame;
+}
+
+
+
+int VideoInput_InitializeLibrary(int numofinputs)
+{
+    //if (total_cameras>0) { fprintf(stderr,"Error , Video Inputs already active ?\n total_cameras=%u\n",total_cameras); return 0;}
+
+    //ReallocEmptyFrame(largest_feed_x,largest_feed_y);
+
+
+    /*First allocate memory for V4L2 Structures  , etc*/
+    camera_feeds = (struct Video * ) malloc ( sizeof( struct Video ) * (numofinputs+1) );
+    if (camera_feeds==0) { fprintf(stderr,"Error , cannot allocate memory for %u video inputs \n",total_cameras); return 0;}
+
+    int i;
+    for ( i=0; i<total_cameras; i++ )
+      {  /*We mark each camera as dead , to preserve a clean state*/
+          camera_feeds[i].loop_thread = 0;
+
+          camera_feeds[i].thread_alive_flag=0;
+         // camera_feeds[i].rec_video.pixels=0;
+          camera_feeds[i].frame=0;
+          //camera_feeds[i].v4l2_intf=0;
+          memset(&camera_feeds[i].v4l2_interface , 0 , sizeof (struct V4L2_c_interface)) ;
+          camera_feeds[i].fx=0,camera_feeds[i].fy=0,camera_feeds[i].cx=0,camera_feeds[i].cy=0;
+          camera_feeds[i].k1=0,camera_feeds[i].k2=0,camera_feeds[i].p1=0,camera_feeds[i].p2=0,camera_feeds[i].k3=0;
+
+          //memset ((void*) camera_feeds[i],0,sizeof(struct Video));
+      }
+
+    /*Lets Refresh USB devices list :)*/
+    if (VIDEOINPUT_DEBUG)
+    {
+      int ret=system((const char * ) "ls /dev/video*");
+      if ( ret == 0 ) { printf("These are the possible video devices .. \n"); }
+
+      ret=system((const char * ) "ls /dev/video* | wc -l");
+      if ( ret == 0 ) { printf("total video devices .. \n"); }
+    }
+
+
+
+    total_cameras=numofinputs;
+
+    return 1 ;
+}
+
+int VideoInput_DeinitializeLibrary()
+{
+    if (total_cameras==0) { fprintf(stderr,"Error , Video Inputs already deactivated ?\n"); return 0;}
+    if (camera_feeds==0) { fprintf(stderr,"Error , Video Inputs already deactivated ?\n"); return 0;}
+
+    if ( empty_frame != 0 ) { free(empty_frame); empty_frame=0; }
+
+    int i=0;
+    for ( i=0; i<total_cameras; i++ )
+     {
+       if (camera_feeds[i].thread_alive_flag)
+       {
+        fprintf(stderr,"Video %u Stopping\n",i);
+        camera_feeds[i].stop_snap_loop=1;
+
+        //if ( pthread_join( camera_feeds[i].loop_thread, NULL) != 0 )           { fprintf(stderr,"Error rejoining VideoInput thread \n"); }
+
+        usleep(30);
+        //camera_feeds[i].v4l2_intf->stopCapture();
+        stopCapture_v4l2intf(&camera_feeds[i].v4l2_interface);
+        usleep(30);
+        //camera_feeds[i].v4l2_intf->freeBuffers();
+        freeBuffers_v4l2intf(&camera_feeds[i].v4l2_interface);
+        usleep(30);
+
+        if ( camera_feeds[i].decoded_pixels !=0 )   { free( camera_feeds[i].decoded_pixels );   }
+      //  if ( camera_feeds[i].rec_video.pixels !=0 ) { free( camera_feeds[i].rec_video.pixels ); }
+        //if ( camera_feeds[i].v4l2_intf != 0 )       { delete camera_feeds[i].v4l2_intf; }
+        destroy_v4l2intf(&camera_feeds[i].v4l2_interface);
+
+       } else
+       {
+        fprintf(stderr,"Video Feed %u seems to be already dead , ensuring no memory leaks!\n",i);
+        camera_feeds[i].stop_snap_loop=1;
+        //if ( camera_feeds[i].rec_video.pixels !=0 ) { free( camera_feeds[i].rec_video.pixels ); }
+        //if ( camera_feeds[i].v4l2_intf != 0 )       { delete camera_feeds[i].v4l2_intf; }
+        destroy_v4l2intf(&camera_feeds[i].v4l2_interface);
+
+       }
+     }
+
+
+
+    fprintf(stderr,"Deallocation of Video Structures\n");
+    free(camera_feeds);
+
+    fprintf(stderr,"Video Input successfully deallocated\n");
+    return 1 ;
+}
+
+
+
+char FileExistsVideoInput(char * filename)
+{
+ FILE *fp = fopen(filename,"r");
+ if( fp ) { /* exists */
+            fclose(fp);
+            return 1;
+          }
+          else
+          { /* doesnt exist */ }
+ return 0;
+}
+
+int VideoInput_OpenFeed(int inpt,char * viddev,int width,int height,int bitdepth,int framespersecond,char snapshots_on,struct VideoFeedSettings videosettings)
+{
+   camera_feeds[inpt].video_simulation=NO_VIDEO_AVAILIABLE;
+   printf("Initializing Video Feed %u ( %s ) @ %u/%u \n",inpt,viddev,width,height);
+   //ReallocEmptyFrame(width,height);
+
+   //if (!VideoInputsOk()) return 0;
+   if ( (!FileExistsVideoInput(viddev)) ) { fprintf(stderr,"\n\nCheck for the webcam (%s) returned false..\n PLEASE CONNECT V4L2 COMPATIBLE CAMERA!!!!!\n\n\n",viddev); return 0; }
+
+
+   camera_feeds[inpt].videoinp = viddev; /*i.e. (char *) "/dev/video0";*/
+   camera_feeds[inpt].width = width;
+   camera_feeds[inpt].height = height;
+   camera_feeds[inpt].size_of_frame=width*height*(bitdepth/8);
+   //We will set this at the end..! camera_feeds[inpt].video_simulation=LIVE_ON;
+   camera_feeds[inpt].thread_alive_flag=0;
+   camera_feeds[inpt].snap_paused=0;
+   camera_feeds[inpt].snap_lock=0;
+
+   camera_feeds[inpt].frame_decoded=0;
+   camera_feeds[inpt].decoded_pixels=0;
+
+
+   CLEAR (camera_feeds[inpt].fmt);
+   camera_feeds[inpt].fmt.fmt.pix.width       = width;
+   camera_feeds[inpt].fmt.fmt.pix.height      = height;
+
+
+   /* IF videosettings is null set default capture mode ( VIDEO CAPTURE , YUYV mode , INTERLACED )  */
+   if ( videosettings.EncodingType==0 ) { camera_feeds[inpt].fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE; } else
+                                        { camera_feeds[inpt].fmt.type =  videosettings.EncodingType; }
+
+   if ( videosettings.PixelFormat==0 ) { camera_feeds[inpt].fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV; } else
+                                       { camera_feeds[inpt].fmt.fmt.pix.pixelformat = videosettings.PixelFormat; }
+
+   if ( videosettings.FieldType==0 ) { camera_feeds[inpt].fmt.fmt.pix.field = V4L2_FIELD_NONE; /*V4L2_FIELD_INTERLACED;*/ } else
+                                     { camera_feeds[inpt].fmt.fmt.pix.field =  videosettings.FieldType; }
+
+   camera_feeds[inpt].input_pixel_format=camera_feeds[inpt].fmt.fmt.pix.pixelformat;
+   camera_feeds[inpt].input_pixel_format_bitdepth=bitdepth;
+
+   PrintOutCaptureMode(camera_feeds[inpt].fmt.type);
+   PrintOutPixelFormat(camera_feeds[inpt].fmt.fmt.pix.pixelformat);
+   PrintOutFieldType(camera_feeds[inpt].fmt.fmt.pix.field);
+
+   camera_feeds[inpt].decoded_pixels=0;
+
+
+       if (!VideoFormatImplemented(camera_feeds[inpt].input_pixel_format,camera_feeds[inpt].input_pixel_format_bitdepth))
+       {
+          fprintf(stderr,(char *)"Video format not implemented!!! :S \n");
+       }
+
+       if (VideoFormatNeedsDecoding(camera_feeds[inpt].input_pixel_format,camera_feeds[inpt].input_pixel_format_bitdepth))
+       {
+          /*NEEDS TO DECODE TO RGB 24 , allocate memory*/
+          camera_feeds[inpt].decoded_pixels = (char * ) malloc( (width*height*3) + 1);
+          if (camera_feeds[inpt].decoded_pixels ==0) { fprintf(stderr,"Error allocating memory for DECODER TO RGB 24 structure"); return 0; }
+          memset(camera_feeds[inpt].decoded_pixels, '\0',width*height*3);
+       }
+
+
+
+      fprintf(stderr,(char *)"Starting camera , if it segfaults consider running \nLD_PRELOAD=/usr/lib/libv4l/v4l2convert.so  executable_name\n");
+
+
+      if (!populateAndStart_v4l2intf(&camera_feeds[inpt].v4l2_interface,camera_feeds[inpt].videoinp,io) ) { fprintf(stderr,"Could not populate and start v4l2 interface..\n"); return 0; }
+
+       struct v4l2_capability cap;
+       if (getcap_v4l2intf(&camera_feeds[inpt].v4l2_interface,&cap))
+       {
+           fprintf(stderr,"Device Bus ....... %s \n" ,cap.bus_info);
+           fprintf(stderr,"Card       ....... %s \n" ,cap.card);
+           fprintf(stderr,"Driver     ....... %s \n" ,cap.driver);
+           fprintf(stderr,"Driver Version ... %d.%d.%d\n",cap.version >> 16,(cap.version >> 8) & 0xff,cap.version & 0xff);
+		   fprintf(stderr,"Capabilities ..... 0x%08X\n", cap.capabilities);
+		   fprintf(stderr,"DevCapabilities .. 0x%08X\n", cap.device_caps);
+
+		   //print_video_formats_ext(&camera_feeds[inpt].v4l2_interface,V4L2_BUF_TYPE_VIDEO_CAPTURE);
+		   /*
+		   fprintf(stderr,"%s", cap2s(cap.capabilities).c_str());
+		   if (vcap.capabilities & V4L2_CAP_DEVICE_CAPS)
+              {
+			   printf("\tDevice Caps   : 0x%08X\n", vcap.device_caps);
+			   printf("%s", cap2s(vcap.device_caps).c_str());
+		      }*/
+
+           if ( ! cap.device_caps & V4L2_CAP_TIMEPERFRAME )
+              { fprintf(stderr,"This Device has no framerate changing capabilities ?\n"); framespersecond = 0; } else
+              {
+                //struct v4l2_frmivalenum argp;
+                //getFramerateIntervals_v4l2intf(&camera_feeds[inpt].v4l2_interface,&argp);
+                //fprintf(stderr,"Todo check if our resolution is supported here\n");
+              }
+       }
+
+      if ( setfmt_v4l2intf(&camera_feeds[inpt].v4l2_interface,camera_feeds[inpt].fmt) == 0 )
+                { fprintf(stderr,"Device does not support settings:\n"); return 0; } else
+                 {
+                    if (VIDEOINPUT_DEBUG) { fprintf(stderr,"No errors , starting camera %u / locking memory..!",inpt); }
+
+                    //This doesnt work as expected
+                    if (framespersecond!=0)
+                      {
+                          if (!setFramerate_v4l2intf(&camera_feeds[inpt].v4l2_interface , (unsigned int) framespersecond ) )
+                                              { fprintf(stderr,"Could not set Framerate to %d  ..\n" ,framespersecond);  } else
+                                              { fprintf(stderr,"Set Framerate to %d  ..\n" ,framespersecond);   }
+                      }
+
+                    if ( !initBuffers_v4l2intf(&camera_feeds[inpt].v4l2_interface) ) { fprintf(stderr,"Could not initialize buffers..\n"); return 0; }
+                    if ( !startCapture_v4l2intf(&camera_feeds[inpt].v4l2_interface) ) { fprintf(stderr,"Could not start capture..\n"); return 0; }
+
+                    camera_feeds[inpt].frame = empty_frame;
+                 }
+
+
+    /* INIT MEMORY FOR SNAPSHOTS !*/
+
+
+    /* STARTING VIDEO RECEIVE THREAD!*/
+    camera_feeds[inpt].thread_alive_flag=0; /* <- This will be set to 1 when child process will start :)*/
+    camera_feeds[inpt].stop_snap_loop=0;
+    camera_feeds[inpt].loop_thread=0;
+    camera_feeds[inpt].frameNumber=0;
+
+   // ChooseDifferentSoftFramerate(inpt,framespersecond); // Go for a good old solid PAL 25 fps , ( the PS3 cameras may be snapping at 120fps , but VisualCortex without
+                                           // hardware acceleration can`t go more than 6-8 fps )
+
+
+
+    /*
+    unsigned int waittime=0,MAX_WAIT=10  , SLEEP_PER_LOOP_MILLI = 20 *  1000;// Milliseconds
+    waittime = MAX_WAIT; // <- disable this
+    if (VIDEOINPUT_DEBUG) { printf("Giving some time ( max =  %u ms ) for the receive threads to wake up ",MAX_WAIT*SLEEP_PER_LOOP_MILLI); }
+    while ( ( waittime<MAX_WAIT ) && (camera_feeds[inpt].thread_alive_flag==0) ) {
+                                                                                   if (waittime%10==0) printf(".");
+                                                                                   usleep(SLEEP_PER_LOOP_MILLI);
+                                                                                   ++waittime;
+                                                                                 }
+   */
+
+    if (VIDEOINPUT_DEBUG) { printf("\nInitVideoFeed %u is ok!\n",inpt); }
+
+    camera_feeds[inpt].video_simulation=LIVE_ON;
+
+  return 1;
+}
+
