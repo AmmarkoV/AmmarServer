8e653b3258267d74e3fbf4cc8db2fddf4da3a203
Ammar Qammaz <ammarkov@gmail.com>
2017-04-04 14:38:45 +0300

    changing the hashmap to also do sorting..!

---
+++ src/AmmServerlib/hashmap/testHashmap.c
@@ -0,0 +1,20 @@
+//
+//gcc testHashmap.c hashmap.c -o testHashmap
+//written by Ammar Qammaz
+//
+
+
+#include <stdio.h>     /* files */
+#include <stdlib.h>     /* qsort */
+#include <string.h>     /* memset */
+
+
+#include "hashmap.h"
+ 
+
+int main()
+{
+  hashMap_Test(0);
+  hashMap_Test(1);
+}
+ 
