e81fd24967bacb5e95551fa1472b825ef3a1a081
Ammar Qammaz <ammarkov@gmail.com>
2017-05-01 19:52:34 +0300

    added a stub for future OpenSSL integration..

---
+++ src/AmmServerlib/network/openssl_server.c
@@ -0,0 +1,92 @@
+#include "openssl_server.h"
+
+
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/uio.h>
+
+
+
+#if USE_OPENSSL
+#warning "Please note that OpenSSL is not yet correctly implemented , this is just a stab that will be used when I get time to properly support it"
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+#define PORT 2024
+
+  SSL_CTX *ctx;
+  SSL *ssl;
+
+void InitializeSSL()
+{
+    SSL_load_error_strings();
+    SSL_library_init();
+    OpenSSL_add_all_algorithms();
+}
+
+void DestroySSL()
+{
+    ERR_free_strings();
+    EVP_cleanup();
+}
+
+void ShutdownSSL()
+{
+    SSL_shutdown(ssl);
+    SSL_free(ssl);
+}
+#endif
+
+
+int startOpenSSLServer()
+{
+#if USE_OPENSSL
+  unsigned int clilen = sizeof(struct sockaddr_in);
+  struct sockaddr_in cli_addr;
+  struct sockaddr_in serv_addr;
+
+
+    int sockfd, newsockfd;
+    SSL_CTX *sslctx;
+    SSL *cSSL;
+
+    InitializeSSL();
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd< 0)
+    {
+        //Log and Error
+        return;
+    }
+    struct sockaddr_in saiServerAddress;
+    bzero((char *) &saiServerAddress, sizeof(saiServerAddress));
+    saiServerAddress.sin_family = AF_INET;
+    //saiServerAddress.sin_addr.s_addr = serv_addr;
+    saiServerAddress.sin_port = htons(PORT);
+
+    bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+
+    listen(sockfd,5);
+    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+
+    sslctx = SSL_CTX_new( SSLv23_server_method());
+    SSL_CTX_set_options(sslctx, SSL_OP_SINGLE_DH_USE);
+    int use_cert = SSL_CTX_use_certificate_file(sslctx, "/serverCertificate.pem", SSL_FILETYPE_PEM);
+
+    int use_prv = SSL_CTX_use_PrivateKey_file(sslctx, "/serverCertificate.pem", SSL_FILETYPE_PEM);
+
+    cSSL = SSL_new(sslctx);
+    SSL_set_fd(cSSL, newsockfd );
+//Here is the SSL Accept portion.  Now all reads and writes must use SSL
+    int ssl_err = SSL_accept(cSSL);
+    if(ssl_err <= 0)
+    {
+        //Error occurred, log and close down ssl
+        ShutdownSSL();
+    }
+#endif
+return 1;
+}
--- src/AmmServerlib/version.h
+++ src/AmmServerlib/version.h
@@ -2,10 +2,10 @@
 #define VERSION_H
 
 	//Date Version Types
-	static const char DATE[] = "04";
-	static const char MONTH[] = "04";
+	static const char DATE[] = "01";
+	static const char MONTH[] = "05";
 	static const char YEAR[] = "2017";
-	static const char UBUNTU_VERSION_STYLE[] =  "17.04";
+	static const char UBUNTU_VERSION_STYLE[] =  "17.05";
 	
 	//Software Status
 	static const char STATUS[] =  "Alpha";
@@ -14,17 +14,17 @@
 	//Standard Version Type
 	static const long MAJOR  = 0;
 	static const long MINOR  = 32;
-	static const long BUILD  = 537;
-	static const long REVISION  = 2731;
+	static const long BUILD  = 542;
+	static const long REVISION  = 2765;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 666;
-	#define RC_FILEVERSION 0,32,537,2731
-	#define RC_FILEVERSION_STRING "0, 32, 537, 2731\0"
-	static const char FULLVERSION_STRING [] = "0.32.537.2731";
+	static const long BUILDS_COUNT  = 672;
+	#define RC_FILEVERSION 0,32,542,2765
+	#define RC_FILEVERSION_STRING "0, 32, 542, 2765\0"
+	static const char FULLVERSION_STRING [] = "0.32.542.2765";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 2;
+	static const long BUILD_HISTORY  = 7;
 	
 
 #endif //VERSION_H
