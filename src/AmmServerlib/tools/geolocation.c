6a35e7290e216f6e28fe13c379cd1ee3ad94cd33
Ammar Qammaz <ammarkov@gmail.com>
2017-05-20 23:35:52 +0300

    adding a stub that in the future might resolve IP addresses to countries..

--- src/AmmServerlib/tools/geolocation.c
+++ src/AmmServerlib/tools/geolocation.c
@@ -1,1 +1,12 @@
+#include "geolocation.h"
+
 //http://www.ipdeny.com/ipblocks/
+// wget http://www.ipdeny.com/ipblocks/data/countries/all-zones.tar.gz && tar xvzf all-zones.tar.gz
+// wget http://www.ipdeny.com/ipv6/ipaddresses/blocks/ipv6-all-zones.tar.gz && tar xvzf ipv6-all-zones.tar.gz
+
+
+
+char * getIPCountry(const char * ip)
+{
+ return 0;
+}
---
+++ src/AmmServerlib/tools/geolocation.h
@@ -0,0 +1,12 @@
+/** @file geolocation.h
+* @brief A collection of tools required by the server and gathered here since they do a very specific job
+* @author Ammar Qammaz (AmmarkoV)
+*/
+
+#ifndef GEOLOCATION_H_INCLUDED
+#define GEOLOCATION_H_INCLUDED
+
+
+char * getIPCountry(const char * ip);
+
+#endif // HTTP_TOOLS_H_INCLUDED
