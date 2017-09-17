c405b42c73db4cd7983243b459d9e4ea12abcd84
Ammar Qammaz <ammarkov@gmail.com>
2017-09-17 23:40:26 +0300

    ...

--- enforceBanlist.sh
+++ enforceBanlist.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+STARTDIR=`pwd`
+#Switch to this directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+
 echo "Ip Tables initially"
 sudo iptables -L -n
 sudo iptables -F INPUT
