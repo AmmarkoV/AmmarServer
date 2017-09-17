d38c8edc75bab3c6fb6910e5030bce97e37fe441
Ammar Qammaz <ammarkov@gmail.com>
2017-09-17 04:01:16 +0300

    adding a script to convert from unviewable flv to web standard webm

---
+++ src/Services/MyTube/convertToHTML5Video.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Will convert all .$2 files I find in $1 to webm and include it in $3"
+
+ 
+cd $1
+
+mkdir -p $3
+
+FILES_TO_CONVERT=`ls | grep .$2`
+for f in $FILES_TO_CONVERT
+do 
+ echo "Converting $f" 
+ TARGETNAME=`basename $f .$2`
+ ffmpeg -i $f -vcodec libvpx -acodec libvorbis $3/$TARGETNAME.webm 
+done
+
+
+exit 0
