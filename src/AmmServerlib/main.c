376b00a68e53f17704c7d0a245a4e571e57c273e
Ammar Qammaz <ammarkov@gmail.com>
2017-09-24 23:57:25 +0300

    upload.png causes segfault ?

--- src/AmmServerlib/main.c
+++ src/AmmServerlib/main.c
@@ -279,7 +279,7 @@ int AmmServer_Running(struct AmmServer_Instance * instance)
 
 int AmmServer_DynamicRequestReturnFile(struct AmmServer_DynamicRequest  * rqst,const char * filename)
 {
-  if ( (rqst==0) || (filename==0) || ( strlen(filename)==0) )  { return 0; }
+  if ( (rqst==0) || (rqst->content==0) || (filename==0) || ( strlen(filename)==0) )  { return 0; }
   rqst->contentContainsPathToFileToBeStreamed=1;
   snprintf(rqst->content,rqst->MAXcontentSize,"%s",filename);
   return 1;
