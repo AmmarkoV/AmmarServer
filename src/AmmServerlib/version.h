150566a0ab1eb5b483fe3168d84b00b2e3470d97
Ammar Qammaz <ammarkov@gmail.com>
2017-11-16 00:37:59 +0200

    trying to accomodate POST direct pointer access for rgbdserver ( in RGBDAcquisition repository )

--- src/AmmServerlib/version.h
+++ src/AmmServerlib/version.h
@@ -2,10 +2,10 @@
 #define VERSION_H
 
 	//Date Version Types
-	static const char DATE[] = "22";
-	static const char MONTH[] = "10";
+	static const char DATE[] = "16";
+	static const char MONTH[] = "11";
 	static const char YEAR[] = "2017";
-	static const char UBUNTU_VERSION_STYLE[] =  "17.10";
+	static const char UBUNTU_VERSION_STYLE[] =  "17.11";
 	
 	//Software Status
 	static const char STATUS[] =  "Alpha";
@@ -14,17 +14,17 @@
 	//Standard Version Type
 	static const long MAJOR  = 0;
 	static const long MINOR  = 33;
-	static const long BUILD  = 644;
-	static const long REVISION  = 3271;
+	static const long BUILD  = 646;
+	static const long REVISION  = 3280;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 807;
-	#define RC_FILEVERSION 0,33,644,3271
-	#define RC_FILEVERSION_STRING "0, 33, 644, 3271\0"
-	static const char FULLVERSION_STRING [] = "0.33.644.3271";
+	static const long BUILDS_COUNT  = 808;
+	#define RC_FILEVERSION 0,33,646,3280
+	#define RC_FILEVERSION_STRING "0, 33, 646, 3280\0"
+	static const char FULLVERSION_STRING [] = "0.33.646.3280";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 9;
+	static const long BUILD_HISTORY  = 11;
 	
 
 #endif //VERSION_H
