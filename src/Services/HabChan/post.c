ecee0354cbf0b1fbdb3b7cbeea1ada7f5b7e523c
Ammar Qammaz <ammarkov@gmail.com>
2017-12-10 04:49:04 +0200

    ...

---
+++ src/Services/HabChan/post.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "state.h"
+#include "post.h"
+
+int loadPosts(const char * threadName , struct board * ourBoard , struct thread * ourThread , struct post * ourPosts)
+{
+
+ return 0;
+}
