5b7f736239d8387ef223988ad9997bad73dac0ac
Ammar Qammaz <ammarkov@gmail.com>
2018-02-24 03:10:30 +0200

    time to sleep now.. :P

--- src/AmmServerlib/version.h
+++ src/AmmServerlib/version.h
@@ -14,17 +14,17 @@
 	//Standard Version Type
 	static const long MAJOR  = 0;
 	static const long MINOR  = 34;
-	static const long BUILD  = 749;
-	static const long REVISION  = 3766;
+	static const long BUILD  = 750;
+	static const long REVISION  = 3772;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 911;
-	#define RC_FILEVERSION 0,34,749,3766
-	#define RC_FILEVERSION_STRING "0, 34, 749, 3766\0"
-	static const char FULLVERSION_STRING [] = "0.34.749.3766";
+	static const long BUILDS_COUNT  = 912;
+	#define RC_FILEVERSION 0,34,750,3772
+	#define RC_FILEVERSION_STRING "0, 34, 750, 3772\0"
+	static const char FULLVERSION_STRING [] = "0.34.750.3772";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 14;
+	static const long BUILD_HISTORY  = 15;
 	
 
 #endif //VERSION_H
