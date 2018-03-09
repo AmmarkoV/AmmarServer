83adbcfa7ae814d7464fa09aee6038b1e2a8ca61
Ammar Qammaz <ammarkov@ics.forth.gr>
2018-03-09 23:05:32 +0200

    fixed paths in scripts..

--- src/AmmServerlib/version.h
+++ src/AmmServerlib/version.h
@@ -14,17 +14,17 @@
 	//Standard Version Type
 	static const long MAJOR  = 0;
 	static const long MINOR  = 35;
-	static const long BUILD  = 845;
-	static const long REVISION  = 4286;
+	static const long BUILD  = 846;
+	static const long REVISION  = 4292;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 1010;
-	#define RC_FILEVERSION 0,35,845,4286
-	#define RC_FILEVERSION_STRING "0, 35, 845, 4286\0"
-	static const char FULLVERSION_STRING [] = "0.35.845.4286";
+	static const long BUILDS_COUNT  = 1013;
+	#define RC_FILEVERSION 0,35,846,4292
+	#define RC_FILEVERSION_STRING "0, 35, 846, 4292\0"
+	static const char FULLVERSION_STRING [] = "0.35.846.4292";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 10;
+	static const long BUILD_HISTORY  = 11;
 	
 
 #endif //VERSION_H
