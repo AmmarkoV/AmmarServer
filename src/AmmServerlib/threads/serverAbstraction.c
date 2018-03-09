2fe2f587dac5e661765553e32cefd3166218a2df
Ammar Qammaz <ammarkov@ics.forth.gr>
2018-03-09 11:28:51 +0200

    adding a basic switch..

--- src/AmmServerlib/threads/serverAbstraction.c
+++ src/AmmServerlib/threads/serverAbstraction.c
@@ -11,21 +11,33 @@
 
 int ASRV_StartHTTPServer(struct AmmServer_Instance * instance,const char * ip,unsigned int port,const char * root_path,const char * templates_path)
 {
- return StartThreadedHTTPServer(instance,ip,port,root_path,templates_path);
+ #if USE_LIBEVENT
+ #else
+  return StartThreadedHTTPServer(instance,ip,port,root_path,templates_path);
+ #endif // USE_LIBEVENT
 }
 
 int ASRV_StopHTTPServer(struct AmmServer_Instance * instance)
 {
- return StopThreadedHTTPServer(instance);
+ #if USE_LIBEVENT
+ #else
+  return StopThreadedHTTPServer(instance);
+ #endif // USE_LIBEVENT
 }
 
 int ASRV_HTTPServerIsRunning(struct AmmServer_Instance * instance)
 {
- return ThreadedHTTPServerIsRunning(instance);
+ #if USE_LIBEVENT
+ #else
+  return ThreadedHTTPServerIsRunning(instance);
+ #endif // USE_LIBEVENT
 }
 
 unsigned int ASRV_GetActiveHTTPServerThreads(struct AmmServer_Instance * instance)
 {
- return GetActiveThreadedHTTPServerThreads(instance);
+ #if USE_LIBEVENT
+ #else
+  return GetActiveThreadedHTTPServerThreads(instance);
+ #endif // USE_LIBEVENT
 }
 
