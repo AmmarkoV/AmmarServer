749859257c426554b246a381beec5d4c66b95ee2
Ammar Qammaz <ammarkov@gmail.com>
2018-03-20 17:10:12 +0200

    adding something that I envision replacing ROS msg and ROS services with an ammar server backbone..!

---
+++ src/AmmMessages/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+  if (argc<1) { fprintf(stderr,"Please add a filename string as a parameter\n"); return 1; }
+
+
+  return 0;
+}
