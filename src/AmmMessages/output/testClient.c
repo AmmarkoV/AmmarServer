83f775955be6a22faa06209e237c086697f77094
Ammar Qammaz <ammarkov@ics.forth.gr>
2018-03-22 19:16:59 +0200

    added message stuff to CMake..

--- src/AmmMessages/output/testClient.c
+++ src/AmmMessages/output/testClient.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
     if (argc<3)
     {
        fprintf(stderr,"Please call with parameters.. \nLike : \n");
-       fprintf(stderr,"./testclient 127.0.0.1 8080\n");
+       fprintf(stderr,"./AmmMessagesTestClient 127.0.0.1 8080\n");
        return 1;
     }
 
