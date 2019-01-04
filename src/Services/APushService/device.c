fa6e8893bdf3b6beb9d298d370c924683442d548
Ammar Qammaz <ammarkov@gmail.com>
2019-01-04 17:36:13 +0200

    ...

---
+++ src/Services/APushService/device.c
@@ -0,0 +1,16 @@
+#include "device.h"
+
+
+int getDeviceID(const char * serialNumber,deviceID * devID)
+{
+
+   return 0;
+}
+
+
+
+int markDeviceIDAsUpdated(deviceID * devID)
+{
+
+   return 0;
+}
---
+++ src/Services/APushService/device.h
@@ -0,0 +1,36 @@
+#ifndef DEVICE_H_INCLUDED
+#define DEVICE_H_INCLUDED
+
+#define MAXIMUM_NUMBER_OF_DEVICES 100
+
+typedef  unsigned int deviceID;
+
+struct informationList
+{
+  char alarm;
+  char sensors[8];
+  char temperatures[8];
+  char relays[8];
+};
+
+
+struct deviceObject
+{
+  unsigned int lastContact;
+  char deviceID[32];
+  char devicePrivateKey[32];
+  char devicePublicKey[32];
+  struct informationList info;
+
+};
+
+struct deviceList
+{
+   unsigned int numberOfDevices;
+   struct deviceObject device[MAXIMUM_NUMBER_OF_DEVICES ];
+};
+
+int getDeviceID(const char * serialNumber,deviceID * devID);
+int markDeviceIDAsUpdated(deviceID * devID);
+
+#endif // DEVICE_H_INCLUDED
