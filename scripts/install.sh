8e74f693e6fc857d67a9f2b0c02753e16e6148cd
Ammar Qammaz <ammarkov@ics.forth.gr>
2019-01-18 15:26:10 +0200

    rearranged installation script..

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,7 @@ STARTDIR=`pwd`
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 
-sudo cp ammarserver /etc/init.d/ammarserver
+sudo cp init.d/ammarserver /etc/init.d/ammarserver
 sudo touch /var/log/ammarserver.log
 sudo chown ammar /var/log/ammarserver.log
 sudo update-rc.d ammarserver defaults
@@ -15,7 +15,7 @@ then
  #systemd?
  echo "SystemD bloat detected, registering with it.."
  sudo systemctl daemon-reload
- sudo cp ammarserver.service /lib/systemd/system/
+ sudo cp systemd/ammarserver.service /lib/systemd/system/
 fi
 
 
