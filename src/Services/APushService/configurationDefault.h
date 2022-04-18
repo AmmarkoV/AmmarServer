82d0918397f98e5b4f34c765703812914e9ca0b2
Ammar Qammaz <ammarkov@gmail.com>
2022-04-18 15:22:28 +0300

    some final memos

--- src/Services/APushService/configurationDefault.h
+++ src/Services/APushService/configurationDefault.h
@@ -1,5 +1,6 @@
+//Copy this file as configuration.h and : 
+#define EMAIL_LIST_BIO "local" //<- add here "A@uoc.gr,B@gmail.com,C@uoc.gr,D@gmail.com"
 
-#define EMAIL_LIST_BIO "local"
 #define DEFAULT_USERNAME "admin"
 #define DEFAULT_PASSWORD "admin"
 
