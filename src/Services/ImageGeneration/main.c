79943f8be7eb59a426baf701cbe511b1917f0d7f
Ammar Qammaz <ammarkov@gmail.com>
2022-09-17 18:20:35 +0300

    ...

--- src/Services/ImageGeneration/main.c
+++ src/Services/ImageGeneration/main.c
@@ -25,29 +25,10 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 #include <unistd.h>
 #include "../../AmmServerlib/AmmServerlib.h"
 
-
-#define ENABLE_STOP_PAGE 0
-#if ENABLE_STOP_PAGE
- #warning "if you open http://127.0.0.1/stop.html this will stop the web server of course you don't want this in production so disable this"
-#endif // ENABLE_STOP_PAGE
-
-#define logEcho() fprintf(stderr," Reached %s , %u \n ", __FILE__, __LINE__);
-
-
-#define MAX_BINDING_PORT 65534
-
-#define ENABLE_PASSWORD_PROTECTION 0
-#define ENABLE_CHAT_BOX 0
-#define ENABLE_MONITOR 1
-
-
 #define DEFAULT_BINDING_PORT 8080  // <--- Change this to 80 if you want to bind to the default http port..!
-#define ADMIN_BINDING_PORT 8082
-#define ENABLE_ADMIN_PAGE 0
-
 #define DYNAMIC_PAGES_MEMORY_COMMITED 4096
 #define MAX_QUERY_SIZE 4096
-//char webserver_root[MAX_FILE_PATH]="ammar.gr/"; //<- This is my dev dir.. itshould be commented out or removed in stable release..
+
 char admin_root[MAX_FILE_PATH]="admin_html/"; // <- change this to the directory that contains your content if you dont want to use the default admin_html dir..
 
 #define WEBSERVERROOT "public_html/"
@@ -56,9 +37,9 @@ char templates_root[MAX_FILE_PATH]="public_html/templates/";
 
 
 //------------------------------------------------
-struct AmmServer_RH_Context indexPageContext={0};
-struct AmmServer_RH_Context imageContext={0};
-struct AmmServer_RH_Context form={0};
+struct AmmServer_RH_Context indexPageContext= {0};
+struct AmmServer_RH_Context imageContext= {0};
+struct AmmServer_RH_Context form= {0};
 //------------------------------------------------
 struct AmmServer_MemoryHandler * indexPage=0;
 //------------------------------------------------
@@ -67,61 +48,66 @@ struct AmmServer_Instance  * default_server=0;
 
 void * prepare_image_content_callback(struct AmmServer_DynamicRequest  * rqst)
 {
-  //AmmServer_DynamicRequestReturnMemoryHandler(rqst,indexPage);
-  return 0;
+    //AmmServer_DynamicRequestReturnMemoryHandler(rqst,indexPage);
+    return 0;
 }
 
 void * prepare_index_content_callback(struct AmmServer_DynamicRequest  * rqst)
 {
-  AmmServer_DynamicRequestReturnMemoryHandler(rqst,indexPage);
-  return 0;
+    AmmServer_DynamicRequestReturnMemoryHandler(rqst,indexPage);
+    return 0;
 }
 
 
 //This function prepares the content of  stats context , ( stats.content )
 void * generateImagesBasedOnQuery(struct AmmServer_DynamicRequest  * rqst)
 {
-  //The url , to Long , Short eetc conventions are shit.. :P I should really make them better :p
-  memset(rqst->content,0,DYNAMIC_PAGES_MEMORY_COMMITED);
+    //The url , to Long , Short eetc conventions are shit.. :P I should really make them better :p
+    memset(rqst->content,0,DYNAMIC_PAGES_MEMORY_COMMITED);
 
-  if  (
+    if  (
         _GETexists(rqst,"query")
-      )
+    )
     {
-        char query[MAX_QUERY_SIZE]={0};
+        char query[MAX_QUERY_SIZE]= {0};
         if ( _GETcpy(rqst,"query",query,MAX_QUERY_SIZE) )
-             {
-                fprintf(stderr,"\n\n\n\n\n\nQUERY : %s \n\n\n\n\n\n\n",query);
-             }
-    } else
+        {
+            fprintf(stderr,"\n\n\n\n\n\nQUERY : %s \n\n\n\n\n\n\n",query);
+        }
+    }
+    else
     {
-      strncpy(rqst->content,"<html><head><meta http-equiv=\"refresh\" content=\"0;URL='index.html'\"></head><body>Could not find a name to go to .. </body></html>",rqst->MAXcontentSize);
+        strncpy(rqst->content,"<html><head><meta http-equiv=\"refresh\" content=\"0;URL='index.html'\"></head><body>Could not find a name to go to .. </body></html>",rqst->MAXcontentSize);
     }
 
-  rqst->contentSize=strlen(rqst->content);
-  return 0;
+    rqst->contentSize=strlen(rqst->content);
+    return 0;
 }
 
 
 //This function adds a Resource Handler for the pages stats.html and formtest.html and associates stats , form and their callback functions
 void init_dynamic_content()
 {
-  //--------------------------------------------------------------------------------------------------------------------------------------------
-  AmmServer_AddResourceHandler(default_server,&indexPageContext,"/index.html",4096,0,&prepare_index_content_callback,SAME_PAGE_FOR_ALL_CLIENTS);
-  AmmServer_AddResourceHandler(default_server,&form,"/go",4096,0,&generateImagesBasedOnQuery,SAME_PAGE_FOR_ALL_CLIENTS);
-  //--------------------------------------------------------------------------------------------------------------------------------------------
-  AmmServer_AddResourceHandler(default_server,&imageContext,"/image.png",1024000,0,&prepare_image_content_callback,SAME_PAGE_FOR_ALL_CLIENTS);
-  //--------------------------------------------------------------------------------------------------------------------------------------------
-  indexPage=AmmServer_ReadFileToMemoryHandler("src/Services/ImageGeneration/generation.html");
-  if (indexPage==0) { AmmServer_Error("Could not find Index Page file"); exit(0); }
-  //--------------------------------------------------------------------------------------------------------------------------------------------
+    //--------------------------------------------------------------------------------------------------------------------------------------------
+    AmmServer_AddResourceHandler(default_server,&indexPageContext,"/index.html",4096,0,&prepare_index_content_callback,SAME_PAGE_FOR_ALL_CLIENTS);
+    AmmServer_AddResourceHandler(default_server,&form,"/go",4096,0,&generateImagesBasedOnQuery,SAME_PAGE_FOR_ALL_CLIENTS);
+    //--------------------------------------------------------------------------------------------------------------------------------------------
+    AmmServer_AddResourceHandler(default_server,&imageContext,"/image.png",1024000,0,&prepare_image_content_callback,SAME_PAGE_FOR_ALL_CLIENTS);
+    //--------------------------------------------------------------------------------------------------------------------------------------------
+    indexPage=AmmServer_ReadFileToMemoryHandler("src/Services/ImageGeneration/generation.html");
+    if (indexPage==0)
+    {
+        AmmServer_Error("Could not find Index Page file");
+        exit(0);
+    }
+    //--------------------------------------------------------------------------------------------------------------------------------------------
 
-  //fresh.txt will always be served fresh
-  AmmServer_DoNOTCacheResource(default_server,"1.png");
-  AmmServer_DoNOTCacheResource(default_server,"2.png");
-  AmmServer_DoNOTCacheResource(default_server,"3.png");
-  AmmServer_DoNOTCacheResource(default_server,"4.png");
-  AmmServer_DoNOTCacheResource(default_server,"5.png");
+    //fresh.txt will always be served fresh
+    AmmServer_DoNOTCacheResource(default_server,"1.png");
+    AmmServer_DoNOTCacheResource(default_server,"2.png");
+    AmmServer_DoNOTCacheResource(default_server,"3.png");
+    AmmServer_DoNOTCacheResource(default_server,"4.png");
+    AmmServer_DoNOTCacheResource(default_server,"5.png");
 }
 
 //This function destroys all Resource Handlers and free's all allocated memory..!
@@ -138,16 +124,16 @@ void close_dynamic_content()
 int main(int argc, char *argv[])
 {
     printf("\nAmmar Server %s starting up..\n",AmmServer_Version());
-   //If we have a command line arguments we overwrite our buffers
+    //If we have a command line arguments we overwrite our buffers
 
-   unsigned int i=0;
-   for (i=0; i<argc; i++)
-   {
-    if ((strcmp(argv[i],"--root")==0)&&(argc>i+1))
-      {
-        snprintf(webserver_root,MAX_FILE_PATH,"%s",argv[i+1]);
-      }
-   }
+    unsigned int i=0;
+    for (i=0; i<argc; i++)
+    {
+        if ((strcmp(argv[i],"--root")==0)&&(argc>i+1))
+        {
+            snprintf(webserver_root,MAX_FILE_PATH,"%s",argv[i+1]);
+        }
+    }
 
     //Check binary and header spec
     AmmServer_CheckIfHeaderBinaryAreTheSame(AMMAR_SERVER_HTTP_HEADER_SPEC);
@@ -161,17 +147,21 @@ int main(int argc, char *argv[])
 
 
     default_server = AmmServer_StartWithArgs(
-                                             "imagegeneration",
-                                              argc,argv , //The internal server will use the arguments to change settings
-                                              //If you don't want this look at the AmmServer_Start call
-                                              bindIP,
-                                              port,
-                                              0, /*This means we don't want a specific configuration file*/
-                                              webserver_root,
-                                              templates_root
-                                              );
+                         "imagegeneration",
+                         argc,argv,  //The internal server will use the arguments to change settings
+                         //If you don't want this look at the AmmServer_Start call
+                         bindIP,
+                         port,
+                         0, /*This means we don't want a specific configuration file*/
+                         webserver_root,
+                         templates_root
+                     );
 
-    if (!default_server) { AmmServer_Error("Could not start server , shutting down everything.."); exit(1); }
+    if (!default_server)
+    {
+        AmmServer_Error("Could not start server , shutting down everything..");
+        exit(1);
+    }
 
 
     //Create dynamic content allocations and associate context to the correct files
@@ -179,15 +169,15 @@ int main(int argc, char *argv[])
     //stats.html and formtest.html should be availiable from now on..!
 
 
-         while ( (AmmServer_Running(default_server))  )
-           {
-             //Main thread should just sleep and let the background threads do the hard work..!
-             //In other applications the programmer could use the main thread to do anything he likes..
-             //The only caveat is that he would takeup more CPU time from the server and that he would have to poll
-             //the AmmServer_Running() call once in a while to make sure everything is in order
-             //usleep(60000);
-             sleep(1);
-           }
+    while ( (AmmServer_Running(default_server))  )
+    {
+        //Main thread should just sleep and let the background threads do the hard work..!
+        //In other applications the programmer could use the main thread to do anything he likes..
+        //The only caveat is that he would takeup more CPU time from the server and that he would have to poll
+        //the AmmServer_Running() call once in a while to make sure everything is in order
+        //usleep(60000);
+        sleep(1);
+    }
 
 
     //Delete dynamic content allocations and remove stats.html and formtest.html from the server
